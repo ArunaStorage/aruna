a33b26ba70a9fc913d42a77a3c132e453b8a87e9
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2022-07-28 16:11:16 +0200

    Remove deprecated migrations

--- migrations/2022-07-02-124450_create_collection_labels/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE collection_labels (
-    collection_id UUID NOT NULL,
-    label_id UUID NOT NULL,
-    PRIMARY KEY (collection_id, label_id),
-    CONSTRAINT fk_collection_id FOREIGN KEY (collection_id) REFERENCES collections(id),
-    CONSTRAINT fk_label_id FOREIGN KEY (label_id) REFERENCES labels(id)
-)
