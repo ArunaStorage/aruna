ec432dfab38130b4323faaa376dd6732214e593b
St4NNi <sb@pus.de>
2022-07-29 13:11:34 +0200

    Added enum types and support

--- src/database/models.rs
+++ src/database/models.rs
@@ -1,12 +1,79 @@
+use super::schema::*;
+use diesel_derive_enum::*;
 use uuid;
 
-use super::schema::*;
-//
+#[derive(Debug, DbEnum)]
+#[DieselTypePath = "sql_types::ObjectStatus"]
+pub enum ObjectStatus {
+    INITIALIZING,
+    AVAILABLE,
+    UNAVAILABLE,
+    ERROR,
+}
+
+#[derive(Debug, DbEnum)]
+#[DieselTypePath = "sql_types::EndpointType"]
+pub enum EndpointType {
+    INITIALIZING,
+    AVAILABLE,
+    UNAVAILABLE,
+    ERROR,
+}
+
+#[derive(Debug, DbEnum)]
+#[DieselTypePath = "sql_types::Dataclass"]
+pub enum Dataclass {
+    PUBLIC,
+    PRIVATE,
+    CONFIDENTIAL,
+    PROTECTED,
+}
+
+#[derive(Debug, DbEnum)]
+#[DieselTypePath = "sql_types::SourceType"]
+pub enum SourceType {
+    S3,
+    URL,
+    DOI,
+}
+
+#[derive(Debug, DbEnum)]
+#[DieselTypePath = "sql_types::KeyValueType"]
+pub enum KeyValueType {
+    LABEL,
+    URL,
+}
+
+#[derive(Debug, DbEnum)]
+#[DieselTypePath = "sql_types::IdentityProviderType"]
+pub enum IdentityProviderType {
+    OIDC,
+}
+
+#[derive(Debug, DbEnum)]
+#[DieselTypePath = "sql_types::UserRights"]
+pub enum UserRights {
+    READ,
+    APPEND,
+    MODIFY,
+    WRITE,
+    ADMIN,
+}
+
+#[derive(Debug, DbEnum)]
+#[DieselTypePath = "sql_types::Resources"]
+pub enum Resources {
+    PROJECT,
+    COLLECTION,
+    OBJECT,
+    OBJECT_GROUP,
+}
+
 #[derive(Queryable, Insertable, Identifiable, Debug)]
 pub struct IdentityProvider {
     pub id: uuid::Uuid,
     pub name: String,
-    pub idp_type: String,
+    pub idp_type: IdentityProviderType,
 }
 
 #[derive(Queryable, Insertable, Identifiable, Debug)]
