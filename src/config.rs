45bc705a2b5f0bc73e73b580ee0205a7db505d36
St4NNi <sb@pus.de>
2022-08-25 14:57:33 +0200

    Refactored collection overview db to own struct

--- src/config.rs
+++ src/config.rs
@@ -95,3 +95,9 @@ impl ArunaServerConfig {
         Ok(EnvConfig {})
     }
 }
+
+impl Default for ArunaServerConfig {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- src/database/crud/collection.rs
+++ src/database/crud/collection.rs
@@ -18,6 +18,15 @@ use chrono::Local;
 use diesel::insert_into;
 use diesel::prelude::*;
 
+#[derive(Debug)]
+struct CollectionOverviewDb {
+    coll: Collection,
+    coll_key_value: Option<Vec<CollectionKeyValue>>,
+    coll_req_labels: Option<Vec<RequiredLabel>>,
+    coll_stats: Option<CollectionStat>,
+    coll_version: Option<CollectionVersion>,
+}
+
 impl Database {
     pub fn create_new_collection(
         &self,
@@ -79,60 +88,57 @@ impl Database {
 
         let collection_id = uuid::Uuid::parse_str(&request.collection_id)?;
 
-        let ret_collection = self.pg_connection.get()?.transaction::<Option<(
-            Collection,
-            Option<Vec<CollectionKeyValue>>,
-            Option<Vec<RequiredLabel>>,
-            Option<CollectionStat>,
-            Option<CollectionVersion>,
-        )>, Error, _>(|conn| {
-            let collection_info = col::collections
-                .filter(col::id.eq(collection_id))
-                .first::<models::collection::Collection>(conn)
-                .optional()?;
+        let ret_collection = self
+            .pg_connection
+            .get()?
+            .transaction::<Option<CollectionOverviewDb>, Error, _>(|conn| {
+                let collection_info = col::collections
+                    .filter(col::id.eq(collection_id))
+                    .first::<models::collection::Collection>(conn)
+                    .optional()?;
 
-            match collection_info {
-                Some(coll_info) => {
-                    let collection_key_values = ckv::collection_key_value
-                        .filter(ckv::collection_id.eq(collection_id))
-                        .load::<models::collection::CollectionKeyValue>(conn)
-                        .optional()?;
+                match collection_info {
+                    Some(coll_info) => {
+                        let collection_key_values = ckv::collection_key_value
+                            .filter(ckv::collection_id.eq(collection_id))
+                            .load::<models::collection::CollectionKeyValue>(conn)
+                            .optional()?;
 
-                    let req_labels = rlbl::required_labels
-                        .filter(rlbl::collection_id.eq(collection_id))
-                        .load::<models::collection::RequiredLabel>(conn)
-                        .optional()?;
+                        let req_labels = rlbl::required_labels
+                            .filter(rlbl::collection_id.eq(collection_id))
+                            .load::<models::collection::RequiredLabel>(conn)
+                            .optional()?;
 
-                    let stats = clstats::collection_stats
-                        .filter(clstats::id.eq(collection_id))
-                        .first::<CollectionStat>(conn)
-                        .optional()?;
+                        let stats = clstats::collection_stats
+                            .filter(clstats::id.eq(collection_id))
+                            .first::<CollectionStat>(conn)
+                            .optional()?;
 
-                    if let Some(cl_version) = coll_info.version_id {
-                        let version = clversion::collection_version
-                            .filter(clversion::id.eq(cl_version))
-                            .first::<CollectionVersion>(conn)?;
+                        if let Some(cl_version) = coll_info.version_id {
+                            let version = clversion::collection_version
+                                .filter(clversion::id.eq(cl_version))
+                                .first::<CollectionVersion>(conn)?;
 
-                        Ok(Some((
-                            coll_info,
-                            collection_key_values,
-                            req_labels,
-                            stats,
-                            Some(version),
-                        )))
-                    } else {
-                        Ok(Some((
-                            coll_info,
-                            collection_key_values,
-                            req_labels,
-                            stats,
-                            None,
-                        )))
+                            Ok(Some(CollectionOverviewDb {
+                                coll: coll_info,
+                                coll_key_value: collection_key_values,
+                                coll_req_labels: req_labels,
+                                coll_stats: stats,
+                                coll_version: Some(version),
+                            }))
+                        } else {
+                            Ok(Some(CollectionOverviewDb {
+                                coll: coll_info,
+                                coll_key_value: collection_key_values,
+                                coll_req_labels: req_labels,
+                                coll_stats: stats,
+                                coll_version: None,
+                            }))
+                        }
                     }
+                    None => Ok(None),
                 }
-                None => Ok(None),
-            }
-        })?;
+            })?;
 
         Ok(GetCollectionByIdResponse {
             collection: map_to_collection_overview(ret_collection)?,
@@ -156,115 +162,110 @@ impl Database {
 
         let project_id = uuid::Uuid::parse_str(&request.project_id)?;
 
-        let ret_collections = self.pg_connection.get()?.transaction::<Option<
-            Vec<(
-                Collection,
-                Option<Vec<CollectionKeyValue>>,
-                Option<Vec<RequiredLabel>>,
-                Option<CollectionStat>,
-                Option<CollectionVersion>,
-            )>,
-        >, Error, _>(|conn| {
-            let mut base_request = col::collections
-                .filter(col::project_id.eq(project_id))
-                .into_boxed();
+        let ret_collections = self
+            .pg_connection
+            .get()?
+            .transaction::<Option<Vec<CollectionOverviewDb>>, Error, _>(|conn| {
+                let mut base_request = col::collections
+                    .filter(col::project_id.eq(project_id))
+                    .into_boxed();
 
-            let mut return_vec: Vec<(
-                Collection,
-                Option<Vec<CollectionKeyValue>>,
-                Option<Vec<RequiredLabel>>,
-                Option<CollectionStat>,
-                Option<CollectionVersion>,
-            )> = Vec::new();
+                let mut return_vec: Vec<CollectionOverviewDb> = Vec::new();
 
-            if let Some(pg_size) = pagesize {
-                base_request = base_request.limit(pg_size);
-            }
+                if let Some(pg_size) = pagesize {
+                    base_request = base_request.limit(pg_size);
+                }
 
-            if let Some(l_uid) = last_uuid {
-                base_request = base_request.filter(col::id.ge(l_uid));
-            }
+                if let Some(l_uid) = last_uuid {
+                    base_request = base_request.filter(col::id.ge(l_uid));
+                }
 
-            if let Some(p_query) = parsed_query {
-                match p_query {
-                    ParsedQuery::LabelQuery(l_query) => {
-                        let mut ckv_query = ckv::collection_key_value.into_boxed();
+                if let Some(p_query) = parsed_query {
+                    match p_query {
+                        ParsedQuery::LabelQuery(l_query) => {
+                            let mut ckv_query = ckv::collection_key_value.into_boxed();
 
-                        // Is "and"
-                        if l_query.1 {
-                            for (key, value) in l_query.0 {
-                                ckv_query = ckv_query.filter(ckv::key.eq(key));
-                                if let Some(val) = value {
-                                    ckv_query = ckv_query.filter(ckv::value.eq(val))
-                                };
+                            // Is "and"
+                            if l_query.1 {
+                                for (key, value) in l_query.0 {
+                                    ckv_query = ckv_query.filter(ckv::key.eq(key));
+                                    if let Some(val) = value {
+                                        ckv_query = ckv_query.filter(ckv::value.eq(val))
+                                    };
+                                }
+                            } else {
+                                for (key, value) in l_query.0 {
+                                    ckv_query = ckv_query.or_filter(ckv::key.eq(key));
+                                    if let Some(val) = value {
+                                        ckv_query = ckv_query.or_filter(ckv::value.eq(val))
+                                    };
+                                }
                             }
-                        } else {
-                            for (key, value) in l_query.0 {
-                                ckv_query = ckv_query.or_filter(ckv::key.eq(key));
-                                if let Some(val) = value {
-                                    ckv_query = ckv_query.or_filter(ckv::value.eq(val))
-                                };
+
+                            let found_cols: Option<Vec<uuid::Uuid>> = ckv_query
+                                .select(ckv::collection_id)
+                                .load::<uuid::Uuid>(conn)
+                                .optional()?;
+
+                            if let Some(fcolls) = found_cols {
+                                base_request = base_request.filter(col::id.eq_any(fcolls))
+                            } else {
+                                return Ok(None);
                             }
                         }
+                        ParsedQuery::IdsQuery(ids) => {
+                            base_request = base_request.filter(col::id.eq_any(ids));
+                        }
+                    };
+                };
 
-                        let found_cols: Option<Vec<uuid::Uuid>> = ckv_query
-                            .select(ckv::collection_id)
-                            .load::<uuid::Uuid>(conn)
+                let query_collections: Option<Vec<Collection>> =
+                    base_request.load::<Collection>(conn).optional()?;
+
+                if let Some(q_colls) = query_collections {
+                    for col in q_colls {
+                        let collection_key_values = ckv::collection_key_value
+                            .filter(ckv::collection_id.eq(col.id))
+                            .load::<models::collection::CollectionKeyValue>(conn)
                             .optional()?;
 
-                        if let Some(fcolls) = found_cols {
-                            base_request = base_request.filter(col::id.eq_any(fcolls))
+                        let req_labels = rlbl::required_labels
+                            .filter(rlbl::collection_id.eq(col.id))
+                            .load::<models::collection::RequiredLabel>(conn)
+                            .optional()?;
+
+                        let stats = clstats::collection_stats
+                            .filter(clstats::id.eq(col.id))
+                            .first::<CollectionStat>(conn)
+                            .optional()?;
+
+                        if let Some(cl_version) = col.version_id {
+                            let version = clversion::collection_version
+                                .filter(clversion::id.eq(cl_version))
+                                .first::<CollectionVersion>(conn)?;
+
+                            return_vec.push(CollectionOverviewDb {
+                                coll: col,
+                                coll_key_value: collection_key_values,
+                                coll_req_labels: req_labels,
+                                coll_stats: stats,
+                                coll_version: Some(version),
+                            });
                         } else {
-                            return Ok(None);
+                            return_vec.push(CollectionOverviewDb {
+                                coll: col,
+                                coll_key_value: collection_key_values,
+                                coll_req_labels: req_labels,
+                                coll_stats: stats,
+                                coll_version: None,
+                            });
                         }
                     }
-                    ParsedQuery::IdsQuery(ids) => {
-                        base_request = base_request.filter(col::id.eq_any(ids));
-                    }
-                };
-            };
-
-            let query_collections: Option<Vec<Collection>> =
-                base_request.load::<Collection>(conn).optional()?;
-
-            if let Some(q_colls) = query_collections {
-                for col in q_colls {
-                    let collection_key_values = ckv::collection_key_value
-                        .filter(ckv::collection_id.eq(col.id))
-                        .load::<models::collection::CollectionKeyValue>(conn)
-                        .optional()?;
-
-                    let req_labels = rlbl::required_labels
-                        .filter(rlbl::collection_id.eq(col.id))
-                        .load::<models::collection::RequiredLabel>(conn)
-                        .optional()?;
-
-                    let stats = clstats::collection_stats
-                        .filter(clstats::id.eq(col.id))
-                        .first::<CollectionStat>(conn)
-                        .optional()?;
-
-                    if let Some(cl_version) = col.version_id {
-                        let version = clversion::collection_version
-                            .filter(clversion::id.eq(cl_version))
-                            .first::<CollectionVersion>(conn)?;
-
-                        return_vec.push((
-                            col,
-                            collection_key_values,
-                            req_labels,
-                            stats,
-                            Some(version),
-                        ));
-                    } else {
-                        return_vec.push((col, collection_key_values, req_labels, stats, None));
-                    }
+                    Ok(Some(return_vec))
+                } else {
+                    Ok(None)
                 }
-                Ok(Some(return_vec))
-            } else {
-                Ok(None)
-            }
-        })?;
+            })?;
 
         let coll_overviews = match ret_collections {
             Some(colls) => {
@@ -310,53 +311,48 @@ impl Database {
 /// * Result<Option<CollectionOverview>, ArunaError>: Returns an Option<CollectionOverview> (gRPC) or ArunaError
 ///
 fn map_to_collection_overview(
-    coll_infos: Option<(
-        Collection,
-        Option<Vec<CollectionKeyValue>>,
-        Option<Vec<RequiredLabel>>,
-        Option<CollectionStat>,
-        Option<CollectionVersion>,
-    )>,
+    coll_infos: Option<CollectionOverviewDb>,
 ) -> Result<Option<CollectionOverview>, ArunaError> {
     if let Some(ret_coll) = coll_infos {
-        let (labels, hooks) = if let Some(ret_kv) = ret_coll.1 {
+        let (labels, hooks) = if let Some(ret_kv) = ret_coll.coll_key_value {
             from_collection_key_values(ret_kv)
         } else {
             (Vec::new(), Vec::new())
         };
 
-        let label_ont = ret_coll.2.map(|req_labels| LabelOntology {
+        let label_ont = ret_coll.coll_req_labels.map(|req_labels| LabelOntology {
             required_label_keys: req_labels
                 .iter()
                 .map(|val| val.label_key.to_string())
                 .collect::<Vec<String>>(),
         });
 
-        let tstmpt = naivedatetime_to_prost_time(ret_coll.0.created_at)?;
+        let tstmpt = naivedatetime_to_prost_time(ret_coll.coll.created_at)?;
 
-        let stats: Result<Option<CollectionStats>, ArunaError> = if let Some(sts) = ret_coll.3 {
-            let obj_stats = Stats {
-                count: sts.object_count,
-                acc_size: sts.size,
+        let stats: Result<Option<CollectionStats>, ArunaError> =
+            if let Some(sts) = ret_coll.coll_stats {
+                let obj_stats = Stats {
+                    count: sts.object_count,
+                    acc_size: sts.size,
+                };
+
+                let coll_stats = CollectionStats {
+                    object_stats: Some(obj_stats),
+                    object_group_count: sts.object_group_count,
+                    last_updated: Some(naivedatetime_to_prost_time(sts.last_updated)?),
+                };
+
+                Ok(Some(coll_stats))
+            } else {
+                Ok(None)
             };
 
-            let coll_stats = CollectionStats {
-                object_stats: Some(obj_stats),
-                object_group_count: sts.object_group_count,
-                last_updated: Some(naivedatetime_to_prost_time(sts.last_updated)?),
-            };
-
-            Ok(Some(coll_stats))
-        } else {
-            Ok(None)
+        let is_public = match ret_coll.coll.dataclass {
+            Some(models::enums::Dataclass::PUBLIC) => true,
+            _ => false,
         };
 
-        let is_public = match ret_coll.0.dataclass {
-            Some(dclass) => matches!(dclass, models::enums::Dataclass::PUBLIC),
-            None => false,
-        };
-
-        let mapped_version = match ret_coll.4 {
+        let mapped_version = match ret_coll.coll_version {
             Some(vers) => Some(collection_overview::Version::SemanticVersion(Version {
                 major: vers.major as i32,
                 minor: vers.minor as i32,
@@ -366,9 +362,9 @@ fn map_to_collection_overview(
         };
 
         Ok(Some(CollectionOverview {
-            id: ret_coll.0.id.to_string(),
-            name: ret_coll.0.name,
-            description: ret_coll.0.description,
+            id: ret_coll.coll.id.to_string(),
+            name: ret_coll.coll.name,
+            description: ret_coll.coll.description,
             labels,
             hooks,
             label_ontology: label_ont,
