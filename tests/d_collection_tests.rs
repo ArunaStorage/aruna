1ee0c99ac9b21145c87f35eedde9d5e16efd085d
St4NNi <sb@pus.de>
2022-09-15 14:29:59 +0200

    Finished first iteration of collection_tests

--- tests/d_collection_tests.rs
+++ tests/d_collection_tests.rs
@@ -111,7 +111,7 @@ fn get_collection_by_id_test() {
     );
 
     assert!(
-        q_col.collection.clone().unwrap().labels[0]
+        q_col.collection.unwrap().labels[0]
             == KeyValue {
                 key: "label_test_key".to_owned(),
                 value: "label_test_value".to_owned(),
@@ -275,15 +275,7 @@ fn get_collections_test() {
     };
     // Expect all
     let quest_result = db.get_collections(q_col_req).unwrap();
-    assert!(
-        quest_result
-            .collections
-            .clone()
-            .unwrap()
-            .collection_overviews
-            .len()
-            == 3
-    );
+    assert!(quest_result.collections.unwrap().collection_overviews.len() == 3);
 
     // 2. Label filter (4)
     let q_col_req = GetCollectionsRequest {
@@ -343,7 +335,6 @@ fn get_collections_test() {
     let quest_result_1 = db.get_collections(q_col_req).unwrap();
     assert!(
         quest_result_1
-            .clone()
             .collections
             .clone()
             .unwrap()
@@ -381,9 +372,8 @@ fn get_collections_test() {
     let quest_result_2 = db.get_collections(q_col_req).unwrap();
     assert!(
         quest_result_2
-            .clone()
             .collections
-            .clone()
+            .as_ref()
             .unwrap()
             .collection_overviews
             .len()
@@ -392,16 +382,11 @@ fn get_collections_test() {
     assert!(
         quest_result_1
             .collections
-            .clone()
+            .as_ref()
             .unwrap()
             .collection_overviews[0]
             .id
-            != quest_result_2
-                .collections
-                .clone()
-                .unwrap()
-                .collection_overviews[0]
-                .id
+            != quest_result_2.collections.unwrap().collection_overviews[0].id
     );
 
     // INVALID
@@ -419,11 +404,7 @@ fn get_collections_test() {
             ids: vec![res_2_id],
         }),
         page_request: Some(PageRequest {
-            last_uuid: quest_result_1
-                .clone()
-                .collections
-                .unwrap()
-                .collection_overviews[0]
+            last_uuid: quest_result_1.collections.unwrap().collection_overviews[0]
                 .clone()
                 .id,
             page_size: 1,
@@ -622,4 +603,344 @@ fn update_collection_test() {
     let pin_up_res = db.update_collection(pin_update, creator).unwrap();
 
     assert!(pin_up_res.collection.unwrap().id != col_id.to_string());
+}
+
+#[test]
+#[ignore]
+#[serial(db)]
+fn pin_collection_test() {
+    let db = database::connection::Database::new("postgres://root:test123@localhost:26257/test");
+
+    let creator = uuid::Uuid::parse_str("12345678-1234-1234-1234-111111111111").unwrap();
+
+    let request = CreateNewCollectionRequest {
+        name: "new_collection_update".to_owned(),
+        description: "this_is_a_demo_collection_update".to_owned(),
+        project_id: "12345678-1111-1111-1111-111111111111".to_owned(),
+        labels: vec![KeyValue {
+            key: "test_key".to_owned(),
+            value: "test_value".to_owned(),
+        }],
+        hooks: vec![KeyValue {
+            key: "test_key".to_owned(),
+            value: "test_value".to_owned(),
+        }],
+        dataclass: 2,
+    };
+
+    let result = db.create_new_collection(request, creator).unwrap();
+    let col_id = uuid::Uuid::from_str(&result.collection_id).unwrap();
+    assert!(!col_id.is_nil());
+
+    let endpoint_uuid = uuid::Uuid::parse_str("12345678-6666-6666-6666-999999999999").unwrap();
+
+    // Add some objects and an objectgroup
+    let new_obj_1 = InitializeNewObjectRequest {
+        object: Some(StageObject {
+            filename: "test_obj_1".to_string(),
+            description: "test_obj_1_descr".to_string(),
+            collection_id: col_id.to_string(),
+            content_len: 5,
+            source: None,
+            dataclass: 2,
+            labels: vec![KeyValue {
+                key: "obj_1_key".to_string(),
+                value: "obj_1_value".to_string(),
+            }],
+            hooks: Vec::new(),
+        }),
+        collection_id: col_id.to_string(),
+        preferred_endpoint_id: endpoint_uuid.to_string(),
+        multipart: false,
+        is_specification: false,
+    };
+    let obj_1_id = uuid::Uuid::new_v4();
+
+    let _sobj_1 = db
+        .create_object(
+            &new_obj_1,
+            &creator,
+            &(Location {
+                r#type: 2,
+                bucket: "a".to_string(),
+                path: "b".to_string(),
+            }),
+            "uid".to_string(),
+            endpoint_uuid,
+            obj_1_id,
+        )
+        .unwrap();
+    let f_obj_1_stage = FinishObjectStagingRequest {
+        object_id: obj_1_id.to_string(),
+        upload_id: "uid".to_string(),
+        collection_id: col_id.to_string(),
+        hash: None,
+        no_upload: false,
+        completed_parts: Vec::new(),
+        auto_update: true,
+    };
+
+    let _res_1 = db.finish_object_staging(&f_obj_1_stage, &creator).unwrap();
+
+    // Add some objects and an objectgroup
+    let new_obj_2 = InitializeNewObjectRequest {
+        object: Some(StageObject {
+            filename: "test_obj_2".to_string(),
+            description: "test_obj_2_descr".to_string(),
+            collection_id: col_id.to_string(),
+            content_len: 10,
+            source: None,
+            dataclass: 2,
+            labels: vec![KeyValue {
+                key: "obj_2_key".to_string(),
+                value: "obj_2_value".to_string(),
+            }],
+            hooks: Vec::new(),
+        }),
+        collection_id: col_id.to_string(),
+        preferred_endpoint_id: endpoint_uuid.to_string(),
+        multipart: false,
+        is_specification: false,
+    };
+
+    let obj_2_id = uuid::Uuid::new_v4();
+
+    let _sobj_2 = db
+        .create_object(
+            &new_obj_2,
+            &creator,
+            &(Location {
+                r#type: 2,
+                bucket: "a".to_string(),
+                path: "b".to_string(),
+            }),
+            "uid_2".to_string(),
+            endpoint_uuid,
+            obj_2_id,
+        )
+        .unwrap();
+    let _f_obj_2_stage = FinishObjectStagingRequest {
+        object_id: obj_2_id.to_string(),
+        upload_id: "uid".to_string(),
+        collection_id: col_id.to_string(),
+        hash: None,
+        no_upload: false,
+        completed_parts: Vec::new(),
+        auto_update: true,
+    };
+
+    let _res_2 = db.finish_object_staging(&f_obj_1_stage, &creator).unwrap();
+
+    let obj_grp = CreateObjectGroupRequest {
+        name: "test_group_1".to_string(),
+        description: "test_group_2".to_string(),
+        collection_id: col_id.to_string(),
+        object_ids: vec![obj_1_id.to_string()],
+        meta_object_ids: Vec::new(),
+        labels: vec![KeyValue {
+            key: "obj_grp_key".to_string(),
+            value: "obj_grp_value".to_string(),
+        }],
+        hooks: Vec::new(),
+    };
+
+    let _obj_grp_res = db.create_object_group(&obj_grp, &creator).unwrap();
+
+    let pin_col_req = PinCollectionVersionRequest {
+        collection_id: col_id.to_string(),
+        version: Some(Version {
+            major: 1,
+            minor: 3,
+            patch: 3,
+        }),
+    };
+    let pin_up_res = db.pin_collection_version(pin_col_req, creator).unwrap();
+
+    assert!(pin_up_res.collection.unwrap().id != col_id.to_string());
+}
+
+#[test]
+#[ignore]
+#[serial(db)]
+fn delete_collection_test() {
+    let db = database::connection::Database::new("postgres://root:test123@localhost:26257/test");
+
+    let creator = uuid::Uuid::parse_str("12345678-1234-1234-1234-111111111111").unwrap();
+
+    let request = CreateNewCollectionRequest {
+        name: "new_collection_update_delete".to_owned(),
+        description: "this_is_a_demo_collection_delete".to_owned(),
+        project_id: "12345678-1111-1111-1111-111111111111".to_owned(),
+        labels: vec![KeyValue {
+            key: "delete_test_key".to_owned(),
+            value: "delete_test_value".to_owned(),
+        }],
+        hooks: vec![KeyValue {
+            key: "delete_test_key".to_owned(),
+            value: "delete_test_value".to_owned(),
+        }],
+        dataclass: 4,
+    };
+
+    let result = db.create_new_collection(request, creator).unwrap();
+
+    let ref_col_request = CreateNewCollectionRequest {
+        name: "new_collection_update_delete".to_owned(),
+        description: "this_is_a_demo_collection_delete".to_owned(),
+        project_id: "12345678-1111-1111-1111-111111111111".to_owned(),
+        labels: vec![KeyValue {
+            key: "delete_test_key".to_owned(),
+            value: "delete_test_value".to_owned(),
+        }],
+        hooks: vec![KeyValue {
+            key: "delete_test_key".to_owned(),
+            value: "delete_test_value".to_owned(),
+        }],
+        dataclass: 4,
+    };
+
+    let result_2 = db.create_new_collection(ref_col_request, creator).unwrap();
+    let col_id = uuid::Uuid::from_str(&result.collection_id).unwrap();
+    assert!(!col_id.is_nil());
+
+    let endpoint_uuid = uuid::Uuid::parse_str("12345678-6666-6666-6666-999999999999").unwrap();
+
+    // Add some objects and an objectgroup
+    let new_obj_1 = InitializeNewObjectRequest {
+        object: Some(StageObject {
+            filename: "test_obj_1_del".to_string(),
+            description: "test_obj_1_descr_del".to_string(),
+            collection_id: col_id.to_string(),
+            content_len: 5,
+            source: None,
+            dataclass: 2,
+            labels: vec![KeyValue {
+                key: "obj_1_key".to_string(),
+                value: "obj_1_value".to_string(),
+            }],
+            hooks: Vec::new(),
+        }),
+        collection_id: col_id.to_string(),
+        preferred_endpoint_id: endpoint_uuid.to_string(),
+        multipart: false,
+        is_specification: false,
+    };
+    let obj_1_id = uuid::Uuid::new_v4();
+
+    let _sobj_1 = db
+        .create_object(
+            &new_obj_1,
+            &creator,
+            &(Location {
+                r#type: 2,
+                bucket: "a".to_string(),
+                path: "b".to_string(),
+            }),
+            "uid".to_string(),
+            endpoint_uuid,
+            obj_1_id,
+        )
+        .unwrap();
+    let f_obj_1_stage = FinishObjectStagingRequest {
+        object_id: obj_1_id.to_string(),
+        upload_id: "uid".to_string(),
+        collection_id: col_id.to_string(),
+        hash: None,
+        no_upload: false,
+        completed_parts: Vec::new(),
+        auto_update: true,
+    };
+
+    let _res_1 = db.finish_object_staging(&f_obj_1_stage, &creator).unwrap();
+
+    let obj_ref_req = CreateObjectReferenceRequest {
+        object_id: obj_1_id.to_string(),
+        collection_id: result.collection_id,
+        target_collection_id: result_2.collection_id,
+        writeable: true,
+        auto_update: true,
+    };
+    let _obj_ref = db.create_object_reference(obj_ref_req);
+    // Add some objects and an objectgroup
+    let new_obj_2 = InitializeNewObjectRequest {
+        object: Some(StageObject {
+            filename: "test_obj_2".to_string(),
+            description: "test_obj_2_descr".to_string(),
+            collection_id: col_id.to_string(),
+            content_len: 10,
+            source: None,
+            dataclass: 2,
+            labels: vec![KeyValue {
+                key: "obj_2_key".to_string(),
+                value: "obj_2_value".to_string(),
+            }],
+            hooks: Vec::new(),
+        }),
+        collection_id: col_id.to_string(),
+        preferred_endpoint_id: endpoint_uuid.to_string(),
+        multipart: false,
+        is_specification: false,
+    };
+
+    let obj_2_id = uuid::Uuid::new_v4();
+
+    let _sobj_2 = db
+        .create_object(
+            &new_obj_2,
+            &creator,
+            &(Location {
+                r#type: 2,
+                bucket: "a".to_string(),
+                path: "b".to_string(),
+            }),
+            "uid_2".to_string(),
+            endpoint_uuid,
+            obj_2_id,
+        )
+        .unwrap();
+    let _f_obj_2_stage = FinishObjectStagingRequest {
+        object_id: obj_2_id.to_string(),
+        upload_id: "uid".to_string(),
+        collection_id: col_id.to_string(),
+        hash: None,
+        no_upload: false,
+        completed_parts: Vec::new(),
+        auto_update: true,
+    };
+
+    let _res_2 = db.finish_object_staging(&f_obj_1_stage, &creator).unwrap();
+
+    let obj_grp = CreateObjectGroupRequest {
+        name: "test_group_1".to_string(),
+        description: "test_group_2".to_string(),
+        collection_id: col_id.to_string(),
+        object_ids: vec![obj_1_id.to_string()],
+        meta_object_ids: Vec::new(),
+        labels: vec![KeyValue {
+            key: "obj_grp_key".to_string(),
+            value: "obj_grp_value".to_string(),
+        }],
+        hooks: Vec::new(),
+    };
+
+    let _obj_grp_res = db.create_object_group(&obj_grp, &creator).unwrap();
+
+    let delete_req_normal = DeleteCollectionRequest {
+        collection_id: col_id.to_string(),
+        project_id: "12345678-1111-1111-1111-111111111111".to_owned(),
+        force: false,
+    };
+
+    let res = db.delete_collection(delete_req_normal, creator);
+    // This should fail !
+    assert!(res.is_err());
+
+    let delete_req_force = DeleteCollectionRequest {
+        collection_id: col_id.to_string(),
+        project_id: "12345678-1111-1111-1111-111111111111".to_owned(),
+        force: true,
+    };
+
+    // Should not fail
+    let _res = db.delete_collection(delete_req_force, creator).unwrap();
 }
