f357b2f36c5afaa8f43b92530a220645e122b634
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2022-09-27 13:57:36 +0200

    Add missing services on server startup

--- src/server/grpc_server.rs
+++ src/server/grpc_server.rs
@@ -2,10 +2,16 @@ use std::sync::Arc;
 
 use tonic::transport::Server;
 
+use crate::api::aruna::api::storage::services::v1::endpoint_service_server::EndpointServiceServer;
+use crate::api::aruna::api::storage::services::v1::object_group_service_server::ObjectGroupServiceServer;
 use crate::api::aruna::api::storage::services::v1::object_service_server::ObjectServiceServer;
+use crate::api::aruna::api::storage::services::v1::project_service_server::ProjectServiceServer;
 use crate::api::aruna::api::storage::services::v1::user_service_server::UserServiceServer;
 use crate::config::ArunaServerConfig;
 use crate::server::services::authz::Authz;
+use crate::server::services::endpoint::EndpointServiceImpl;
+use crate::server::services::objectgroup::ObjectGroupServiceImpl;
+use crate::server::services::project::ProjectServiceImpl;
 use crate::server::services::user::UserServiceImpl;
 use crate::{
     api::aruna::api::storage::services::v1::collection_service_server::CollectionServiceServer,
@@ -34,17 +40,25 @@ impl ServiceServer {
         // Upstart server
         let addr = "0.0.0.0:50051".parse().unwrap();
         let authz = Arc::new(Authz::new(db_ref.clone()).await);
+
+        let endpoint_service =
+            EndpointServiceImpl::new(db_ref.clone(), authz.clone(), default_endpoint.clone()).await;
+        let project_service = ProjectServiceImpl::new(db_ref.clone(), authz.clone()).await;
+        let user_service = UserServiceImpl::new(db_ref.clone(), authz.clone()).await;
         let collection_service = CollectionServiceImpl::new(db_ref.clone(), authz.clone()).await;
         let object_service =
-            ObjectServiceImpl::new(db_ref.clone(), authz.clone(), default_endpoint).await;
-        let user_service = UserServiceImpl::new(db_ref.clone(), authz.clone()).await;
+            ObjectServiceImpl::new(db_ref.clone(), authz.clone(), default_endpoint.clone()).await;
+        let object_group_service = ObjectGroupServiceImpl::new(db_ref.clone(), authz.clone()).await;
 
         log::info!("ArunaServer listening on {}", addr);
 
         Server::builder()
+            .add_service(EndpointServiceServer::new(endpoint_service))
+            .add_service(UserServiceServer::new(user_service))
+            .add_service(ProjectServiceServer::new(project_service))
             .add_service(CollectionServiceServer::new(collection_service))
             .add_service(ObjectServiceServer::new(object_service))
-            .add_service(UserServiceServer::new(user_service))
+            .add_service(ObjectGroupServiceServer::new(object_group_service))
             .serve(addr)
             .await
             .unwrap();
