ff2145590c456b1ee3b84636fffb5c742da5a70a
St4NNi <sb@pus.de>
2022-10-27 14:09:31 +0200

    Fixed linting

--- src/database/crud/cron.rs
+++ src/database/crud/cron.rs
@@ -5,8 +5,7 @@ use crate::{database::connection::Database, error::ArunaError};
 impl Database {
     pub fn update_collection_views(&self) -> Result<(), ArunaError> {
         // Update materialized views
-        self
-            .pg_connection
+        self.pg_connection
             .get()?
             .transaction::<_, ArunaError, _>(|conn| {
                 sql_query("REFRESH MATERIALIZED VIEW collection_stats").execute(conn)?;
@@ -16,8 +15,7 @@ impl Database {
 
     pub fn update_object_group_views(&self) -> Result<(), ArunaError> {
         // Update materialized views
-        self
-            .pg_connection
+        self.pg_connection
             .get()?
             .transaction::<_, ArunaError, _>(|conn| {
                 sql_query("REFRESH MATERIALIZED VIEW object_group_stats").execute(conn)?;
