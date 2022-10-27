2a531349523737db65cfcd952235ef625e9700bf
St4NNi <sb@pus.de>
2022-10-27 15:16:53 +0200

    Hotfix: Materialized view refreshs are not allowed in transactions

--- src/database/crud/cron.rs
+++ src/database/crud/cron.rs
@@ -1,25 +1,19 @@
-use diesel::{sql_query, Connection, RunQueryDsl};
+use diesel::{sql_query, RunQueryDsl};
 
 use crate::{database::connection::Database, error::ArunaError};
 
 impl Database {
     pub fn update_collection_views(&self) -> Result<(), ArunaError> {
         // Update materialized views
-        self.pg_connection
-            .get()?
-            .transaction::<_, ArunaError, _>(|conn| {
-                sql_query("REFRESH MATERIALIZED VIEW collection_stats").execute(conn)?;
-                Ok(())
-            })
+        let mut conn = self.pg_connection.get()?;
+        sql_query("REFRESH MATERIALIZED VIEW collection_stats").execute(&mut conn)?;
+        Ok(())
     }
 
     pub fn update_object_group_views(&self) -> Result<(), ArunaError> {
         // Update materialized views
-        self.pg_connection
-            .get()?
-            .transaction::<_, ArunaError, _>(|conn| {
-                sql_query("REFRESH MATERIALIZED VIEW object_group_stats").execute(conn)?;
-                Ok(())
-            })
+        let mut conn = self.pg_connection.get()?;
+        sql_query("REFRESH MATERIALIZED VIEW object_group_stats").execute(&mut conn)?;
+        Ok(())
     }
 }
