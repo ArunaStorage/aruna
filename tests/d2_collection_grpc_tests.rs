3a3d96740d53028cf5ec3532d628400af2fcb6b3
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2022-12-09 12:27:25 +0100

    test: Add gRPC tests for collection information fetching

--- tests/d2_collection_grpc_tests.rs
+++ tests/d2_collection_grpc_tests.rs
@@ -1,13 +1,15 @@
 extern crate core;
 
+use crate::common::functions::TCreateCollection;
 use aruna_rust_api::api::storage::models::v1::{
-    DataClass, KeyValue, LabelOntology, Permission, ProjectPermission,
+    DataClass, KeyValue, LabelFilter, LabelOntology, LabelOrIdQuery, Permission, ProjectPermission,
 };
 use aruna_rust_api::api::storage::services::v1::collection_service_server::CollectionService;
 use aruna_rust_api::api::storage::services::v1::project_service_server::ProjectService;
 use aruna_rust_api::api::storage::services::v1::user_service_server::UserService;
 use aruna_rust_api::api::storage::services::v1::{
-    AddUserToProjectRequest, CreateNewCollectionRequest, GetCollectionByIdRequest, GetUserRequest,
+    AddUserToProjectRequest, CreateNewCollectionRequest, GetCollectionByIdRequest,
+    GetCollectionsRequest, GetUserRequest,
 };
 use aruna_server::{
     database::{self},
@@ -164,4 +166,384 @@ async fn create_collection_grpc_test() {
             _ => panic!("Unspecified permission is not allowed."),
         }
     }
+}
+
+#[ignore]
+#[tokio::test]
+#[serial(db)]
+async fn get_collection_grpc_test() {
+    // Init project service
+    let db = Arc::new(database::connection::Database::new(
+        "postgres://root:test123@localhost:26257/test",
+    ));
+    let authz = Arc::new(Authz::new(db.clone()).await);
+    let collection_service = CollectionServiceImpl::new(db, authz).await;
+
+    // Fast track project creation
+    let random_project = common::functions::create_project(None);
+
+    // Fast track user adding with None permission
+    let user_id = common::grpc_helpers::get_token_user_id(common::oidc::REGULARTOKEN).await;
+    let add_perm = common::grpc_helpers::add_project_permission(
+        random_project.id.as_str(),
+        user_id.as_str(),
+        common::oidc::ADMINTOKEN,
+    )
+    .await;
+    assert_eq!(add_perm.permission, Permission::None as i32);
+
+    // Fast track collection creation
+    let random_collection = common::functions::create_collection(TCreateCollection {
+        project_id: random_project.id.to_string(),
+        num_labels: 0,
+        num_hooks: 0,
+        col_override: None,
+        creator_id: None,
+    });
+
+    // Get collection information with varying permissions
+    for permission in vec![
+        Permission::None,
+        Permission::Read,
+        Permission::Append,
+        Permission::Modify,
+        Permission::Admin,
+    ]
+    .iter()
+    {
+        // Fast track permission edit
+        let edit_perm = common::grpc_helpers::edit_project_permission(
+            random_project.id.as_str(),
+            user_id.as_str(),
+            permission,
+            common::oidc::ADMINTOKEN,
+        )
+        .await;
+        assert_eq!(edit_perm.permission, *permission as i32);
+
+        // Create gRPC request to fetch collection information
+        let get_collection_grpc_request = common::grpc_helpers::add_token(
+            tonic::Request::new(GetCollectionByIdRequest {
+                collection_id: random_collection.id.to_string(),
+            }),
+            common::oidc::REGULARTOKEN,
+        );
+        let get_collection_response = collection_service
+            .get_collection_by_id(get_collection_grpc_request)
+            .await;
+
+        match *permission {
+            Permission::None => {
+                assert!(get_collection_response.is_err());
+            }
+            Permission::Read | Permission::Append | Permission::Modify | Permission::Admin => {
+                assert!(get_collection_response.is_ok());
+
+                // Validate fetched collection information
+                let collection = get_collection_response
+                    .unwrap()
+                    .into_inner()
+                    .collection
+                    .unwrap();
+
+                assert_eq!(collection.id, random_collection.id);
+                assert_eq!(collection.name, random_collection.name);
+                assert_eq!(collection.description, random_collection.description);
+                assert_eq!(collection.labels, random_collection.labels);
+                assert_eq!(collection.hooks, random_collection.hooks);
+                assert_eq!(collection.label_ontology, random_collection.label_ontology);
+                assert!(collection.is_public)
+            }
+            _ => panic!("Unspecified permission is not allowed."),
+        }
+    }
+}
+
+#[ignore]
+#[tokio::test]
+#[serial(db)]
+async fn get_collections_grpc_test() {
+    // Init project service
+    let db = Arc::new(database::connection::Database::new(
+        "postgres://root:test123@localhost:26257/test",
+    ));
+    let authz = Arc::new(Authz::new(db.clone()).await);
+    let collection_service = CollectionServiceImpl::new(db, authz).await;
+
+    // Fast track project creation
+    let random_project = common::functions::create_project(None);
+
+    // Fast track user adding with None permission
+    let user_id = common::grpc_helpers::get_token_user_id(common::oidc::REGULARTOKEN).await;
+    let add_perm = common::grpc_helpers::add_project_permission(
+        random_project.id.as_str(),
+        user_id.as_str(),
+        common::oidc::ADMINTOKEN,
+    )
+    .await;
+    assert_eq!(add_perm.permission, Permission::None as i32);
+
+    // Create multiple collections with labels for filtering
+    let mut collection_ids: Vec<String> = Vec::new();
+
+    // Loop with label vectors
+    for (index, labels) in vec![
+        vec![
+            KeyValue {
+                key: "station_id".to_string(),
+                value: "12345".to_string(),
+            },
+            KeyValue {
+                key: "deprecated".to_string(),
+                value: "".to_string(),
+            },
+        ],
+        vec![KeyValue {
+            key: "station_id".to_string(),
+            value: "12345".to_string(),
+        }],
+        vec![
+            KeyValue {
+                key: "station_id".to_string(),
+                value: "23456".to_string(),
+            },
+            KeyValue {
+                key: "deprecated".to_string(),
+                value: "".to_string(),
+            },
+        ],
+        vec![KeyValue {
+            key: "station_id".to_string(),
+            value: "23456".to_string(),
+        }],
+        vec![KeyValue {
+            key: "station_id".to_string(),
+            value: "34567".to_string(),
+        }],
+    ]
+    .iter()
+    .enumerate()
+    {
+        let create_collection_request = common::grpc_helpers::add_token(
+            tonic::Request::new(CreateNewCollectionRequest {
+                name: format!("Test Collection 00{}", index).to_string(),
+                description: "Created for get_collections_grpc_test().".to_string(),
+                project_id: random_project.id.to_string(),
+                labels: labels.clone(),
+                hooks: vec![],
+                label_ontology: None,
+                dataclass: DataClass::Private as i32,
+            }),
+            common::oidc::ADMINTOKEN,
+        );
+
+        let response = collection_service
+            .create_new_collection(create_collection_request)
+            .await
+            .unwrap()
+            .into_inner();
+
+        collection_ids.push(response.collection_id);
+    }
+
+    // Request to get all collections
+    let get_all_collections_request = common::grpc_helpers::add_token(
+        tonic::Request::new(GetCollectionsRequest {
+            project_id: random_project.id.to_string(),
+            label_or_id_filter: None,
+            page_request: None,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+
+    let get_all_collections_response = collection_service
+        .get_collections(get_all_collections_request)
+        .await
+        .unwrap()
+        .into_inner();
+    let collections = get_all_collections_response.collections.unwrap();
+
+    assert_eq!(collections.collection_overviews.len(), 5);
+
+    // Request to filter collections by ID
+    let id_filter_collections_request = common::grpc_helpers::add_token(
+        tonic::Request::new(GetCollectionsRequest {
+            project_id: random_project.id.to_string(),
+            label_or_id_filter: Some(LabelOrIdQuery {
+                labels: None,
+                ids: collection_ids[0..3].to_vec(),
+            }),
+            page_request: None,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+
+    let id_filter_collections_response = collection_service
+        .get_collections(id_filter_collections_request)
+        .await
+        .unwrap()
+        .into_inner();
+    let collections = id_filter_collections_response.collections.unwrap();
+
+    assert_eq!(collections.collection_overviews.len(), 3);
+
+    // Request to filter collections by label key+value
+    let label_filter_collections_request = common::grpc_helpers::add_token(
+        tonic::Request::new(GetCollectionsRequest {
+            project_id: random_project.id.to_string(),
+            label_or_id_filter: Some(LabelOrIdQuery {
+                labels: Some(LabelFilter {
+                    labels: vec![KeyValue {
+                        key: "station_id".to_string(),
+                        value: "12345".to_string(),
+                    }],
+                    and_or_or: false,
+                    keys_only: false,
+                }),
+                ids: vec![],
+            }),
+            page_request: None,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+
+    let label_filter_collections_response = collection_service
+        .get_collections(label_filter_collections_request)
+        .await
+        .unwrap()
+        .into_inner();
+    let collections = label_filter_collections_response.collections.unwrap();
+
+    assert_eq!(collections.collection_overviews.len(), 2);
+
+    // Request to filter collections by label key only
+    let label_filter_collections_request = common::grpc_helpers::add_token(
+        tonic::Request::new(GetCollectionsRequest {
+            project_id: random_project.id.to_string(),
+            label_or_id_filter: Some(LabelOrIdQuery {
+                labels: Some(LabelFilter {
+                    labels: vec![KeyValue {
+                        key: "deprecated".to_string(),
+                        value: "".to_string(),
+                    }],
+                    and_or_or: false,
+                    keys_only: true,
+                }),
+                ids: vec![],
+            }),
+            page_request: None,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+
+    let label_filter_collections_response = collection_service
+        .get_collections(label_filter_collections_request)
+        .await
+        .unwrap()
+        .into_inner();
+    let collections = label_filter_collections_response.collections.unwrap();
+
+    assert_eq!(collections.collection_overviews.len(), 2);
+
+    // Loop over permissions to test authorization
+    for permission in vec![
+        Permission::None,
+        Permission::Read,
+        Permission::Append,
+        Permission::Modify,
+        Permission::Admin,
+    ]
+    .iter()
+    {
+        // Fast track permission edit
+        let edit_perm = common::grpc_helpers::edit_project_permission(
+            random_project.id.as_str(),
+            user_id.as_str(),
+            permission,
+            common::oidc::ADMINTOKEN,
+        )
+        .await;
+        assert_eq!(edit_perm.permission, *permission as i32);
+
+        // Request to get all collections
+        let get_all_collections_request = common::grpc_helpers::add_token(
+            tonic::Request::new(GetCollectionsRequest {
+                project_id: random_project.id.to_string(),
+                label_or_id_filter: None,
+                page_request: None,
+            }),
+            common::oidc::REGULARTOKEN,
+        );
+
+        // Try to get unfiltered collections of project
+        let get_collections_response = collection_service
+            .get_collections(get_all_collections_request)
+            .await;
+
+        match *permission {
+            Permission::None => {
+                assert!(get_collections_response.is_err());
+            }
+            Permission::Read | Permission::Append | Permission::Modify | Permission::Admin => {
+                assert!(get_collections_response.is_ok());
+
+                // Validate fetched collection information
+                let collections = get_collections_response
+                    .unwrap()
+                    .into_inner()
+                    .collections
+                    .unwrap();
+
+                assert_eq!(collections.collection_overviews.len(), 5);
+            }
+            _ => panic!("Unspecified permission is not allowed."),
+        }
+    }
+
+    // Get single collection and validate fetched fields
+    let get_single_collections_request = common::grpc_helpers::add_token(
+        tonic::Request::new(GetCollectionsRequest {
+            project_id: random_project.id.to_string(),
+            label_or_id_filter: Some(LabelOrIdQuery {
+                labels: None,
+                ids: collection_ids[0..1].to_vec(),
+            }),
+            page_request: None,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+
+    let get_single_collections_response = collection_service
+        .get_collections(get_single_collections_request)
+        .await
+        .unwrap()
+        .into_inner();
+    let collections = get_single_collections_response
+        .collections
+        .unwrap()
+        .collection_overviews;
+
+    assert_eq!(collections.len(), 1);
+    assert_eq!(collections[0].name, "Test Collection 000".to_string());
+    assert_eq!(
+        collections[0].description,
+        "Created for get_collections_grpc_test().".to_string()
+    );
+    assert_eq!(collections[0].labels.len(), 2);
+    assert!(collections[0].labels.contains(&KeyValue {
+        key: "station_id".to_string(),
+        value: "12345".to_string(),
+    }));
+    assert!(collections[0].labels.contains(&KeyValue {
+        key: "deprecated".to_string(),
+        value: "".to_string(),
+    }));
+    assert_eq!(collections[0].hooks, vec![]);
+    assert_eq!(
+        collections[0].label_ontology,
+        Some(LabelOntology {
+            required_label_keys: vec![]
+        })
+    );
+    assert!(!collections[0].is_public)
 }
