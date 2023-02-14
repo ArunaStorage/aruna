aeabacaa4b3b667de7ed25af76970c27ba4484b7
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2023-02-14 09:26:54 +0100

    test: Add gRPC tests for creating objects with/without custom paths

--- tests/e3_object_path_grpc_tests.rs
+++ tests/e3_object_path_grpc_tests.rs
@@ -1,4 +1,18 @@
+use crate::common::functions::{TCreateCollection, TCreateObject};
+use crate::common::grpc_helpers::get_token_user_id;
+
+use aruna_rust_api::api::storage::models::v1::{DataClass, Hash, Hashalgorithm, Permission};
+use aruna_rust_api::api::storage::services::v1::object_service_server::ObjectService;
+use aruna_rust_api::api::storage::services::v1::{FinishObjectStagingRequest, GetObjectByIdRequest, InitializeNewObjectRequest, StageObject};
+use aruna_server::config::ArunaServerConfig;
+use aruna_server::database;
+use aruna_server::server::services::authz::Authz;
+use aruna_server::server::services::object::ObjectServiceImpl;
+
 use serial_test::serial;
+use std::sync::Arc;
+use aruna_server::database::crud::object::get_object;
+
 mod common;
 
 /// The individual steps of this test function contains:
@@ -8,7 +22,154 @@ mod common;
 #[tokio::test]
 #[serial(db)]
 async fn create_object_with_path_grpc_test() {
-   todo!()
+    // Init database connection
+    let db = Arc::new(database::connection::Database::new(
+        "postgres://root:test123@localhost:26257/test",
+    ));
+    let authz = Arc::new(Authz::new(db.clone()).await);
+
+    // Read test config relative to binary
+    let config = ArunaServerConfig::new();
+
+    // Initialize instance default data proxy endpoint
+    let default_endpoint = db
+        .clone()
+        .init_default_endpoint(config.config.default_endpoint)
+        .unwrap();
+
+    // Init object service
+    let object_service = ObjectServiceImpl::new(db.clone(), authz, default_endpoint).await;
+
+    // Fast track project creation
+    let project_id = common::functions::create_project(None).id;
+
+    // Fast track adding user to project
+    let user_id = get_token_user_id(common::oidc::REGULARTOKEN).await;
+    let add_perm = common::grpc_helpers::add_project_permission(
+        project_id.as_str(),
+        user_id.as_str(),
+        common::oidc::ADMINTOKEN,
+    )
+        .await;
+    assert_eq!(add_perm.permission, Permission::None as i32);
+
+    // Fast track collection creation
+    let collection_meta = TCreateCollection {
+        project_id: project_id.clone(),
+        num_labels: 0,
+        num_hooks: 0,
+        col_override: None,
+        creator_id: Some(user_id.clone()),
+    };
+    let random_collection = common::functions::create_collection(collection_meta.clone());
+
+    // Create random object with default subpath
+    let object_meta = TCreateObject {
+        sub_path: None,
+        creator_id: Some(user_id.clone()),
+        collection_id: random_collection.id.to_string(),
+        default_endpoint_id: None,
+        num_labels: 0,
+        num_hooks: 0,
+    };
+    let random_object = common::functions::create_object(&object_meta);
+
+    // Get object and validate empty default path
+    let get_object_request = common::grpc_helpers::add_token(
+        tonic::Request::new(GetObjectByIdRequest {
+            collection_id: init_object_response.collection_id.clone(),
+            object_id: init_object_response.object_id.clone(),
+            with_url: false,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+    let get_object_response = object_service
+        .get_object_by_id(get_object_request)
+        .await
+        .unwrap()
+        .into_inner();
+
+    let default_object_with_url = get_object_response.object.unwrap();
+    let default_object = default_object_with_url.object.unwrap();
+
+    assert_eq!(default_object.id, random_object.id);
+    assert_eq!(default_object_with_url.paths.len(), 1); // Only empty default path
+    assert!(default_object_with_url.paths.contains(&"".to_string()));
+    assert!(default_object_with_url.url.is_empty());
+
+    // Create object with custom sub_path
+    let init_object_request = common::grpc_helpers::add_token(
+        tonic::Request::new(InitializeNewObjectRequest {
+            object: Some(StageObject {
+                filename: "test.file".to_string(),
+                sub_path: "/some/sub/path".to_string(),
+                content_len: 123456,
+                source: None,
+                dataclass: DataClass::Private as i32,
+                labels: vec![],
+                hooks: vec![],
+            }),
+            collection_id: random_collection.id.to_string(),
+            preferred_endpoint_id: "".to_string(),
+            multipart: false,
+            is_specification: false,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+
+    let init_object_response = object_service
+        .initialize_new_object(init_object_request)
+        .await
+        .unwrap()
+        .into_inner();
+
+    // Finish object
+    let finish_object_request = common::grpc_helpers::add_token(
+        tonic::Request::new(FinishObjectStagingRequest {
+            object_id: init_object_response.object_id.to_string(),
+            upload_id: init_object_response.upload_id.to_string(),
+            collection_id: init_object_response.collection_id.to_string(),
+            hash: Some(Hash {
+                alg: Hashalgorithm::Sha256 as i32,
+                hash:
+                "4ec2d656985e3d823b81cc2cd9b56ec27ab1303cfebaf5f95c37d2fe1661a779"
+                    .to_string(),
+            }),
+            no_upload: false,
+            completed_parts: vec![],
+            auto_update: true,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+
+    let finish_object_response = object_service
+        .finish_object_staging(finish_object_request)
+        .await;
+
+    assert!(finish_object_response.is_ok());
+
+    // Get object and validate path creation
+    let get_object_request = common::grpc_helpers::add_token(
+        tonic::Request::new(GetObjectByIdRequest {
+            collection_id: init_object_response.collection_id.clone(),
+            object_id: init_object_response.object_id.clone(),
+            with_url: false,
+        }),
+        common::oidc::ADMINTOKEN,
+    );
+    let get_object_response = object_service
+        .get_object_by_id(get_object_request)
+        .await
+        .unwrap()
+        .into_inner();
+
+    let custom_object_with_url = get_object_response.object.unwrap();
+    let custom_object = default_object_with_url.object.unwrap();
+
+    assert_eq!(custom_object.id, init_object_response.object_id);
+    assert_eq!(custom_object_with_url.paths.len(), 1); // Only empty default path
+    assert!(custom_object_with_url.paths.contains(&"/my/little/pony".to_string()));
+    assert!(custom_object_with_url.url.is_empty());
 }
 
 /// The individual steps of this test function contains:
