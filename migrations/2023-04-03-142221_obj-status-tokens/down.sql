e4bda050db62b84465665afcccce9dca8bfae269
St4NNi <sb@pus.de>
2023-04-03 16:56:28 +0200

    feat: Update database schema

---
+++ migrations/2023-04-03-142221_obj-status-tokens/down.sql
@@ -0,0 +1,8 @@
+-- This file should undo anything in `up.sql`
+
+ALTER TYPE OBJECT_STATUS DROP VALUE 'FINALIZING';
+DROP TYPE ENDPOINT_STATUS;
+ALTER TABLE endpoints DROP COLUMN status;
+ALTER TABLE users DROP COLUMN is_service_account;
+ALTER TABLE api_tokens DROP COLUMN used_at;
+ALTER TABLE api_tokens DROP COLUMN is_session;
