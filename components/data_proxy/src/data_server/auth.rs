65aa1b31c48ef3d9db903ada5c603edfd8168c90
St4NNi <sb@pus.de>
2023-05-17 14:25:33 +0200

    fix: Open new connection for permission check

--- components/data_proxy/src/data_server/auth.rs
+++ components/data_proxy/src/data_server/auth.rs
@@ -6,28 +6,28 @@ use s3s::{
     auth::{S3Auth, SecretKey},
     s3_error, S3Result,
 };
-use tonic::{transport::Channel, Request};
+use tonic::Request;
 
 /// Aruna authprovider
 #[derive(Debug)]
 pub struct AuthProvider {
-    client: InternalAuthorizeServiceClient<Channel>,
+    aruna_endpoint: tonic::transport::Endpoint,
 }
 
 impl AuthProvider {
-    pub async fn new(aruna_url: impl Into<String>) -> Result<Self> {
-        let client = InternalAuthorizeServiceClient::connect(aruna_url.into()).await?;
-
-        Ok(Self { client })
+    pub async fn new(aruna_url: String) -> Result<Self> {
+        Ok(Self {
+            aruna_endpoint: tonic::transport::Endpoint::try_from(aruna_url)?,
+        })
     }
 }
 
 #[async_trait::async_trait]
 impl S3Auth for AuthProvider {
     async fn get_secret_key(&self, access_key: &str) -> S3Result<SecretKey> {
-        let secret = self
-            .client
-            .clone()
+        let secret = InternalAuthorizeServiceClient::connect(self.aruna_endpoint.clone())
+            .await
+            .map_err(|_| s3_error!(NotSignedUp, "Unable to authenticate user"))?
             .get_secret(Request::new(GetSecretRequest {
                 accesskey: access_key.to_string(),
             }))
