e1ec42a5740f707eed8b7c1d58aad1cdca333378
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2023-06-27 20:10:10 +0200

    refactor: Implement emit_event_with_relation
    This function takes a relation instead of a Vec<EmittedResource> which
    makes it easier to emit events which concern Objects.

--- src/server/clients/event_emit_client.rs
+++ src/server/clients/event_emit_client.rs
@@ -1,14 +1,16 @@
 use aruna_rust_api::api::{
     internal::v1::{
-        internal_event_emitter_service_client::InternalEventEmitterServiceClient, EmitEventRequest,
-        EmitEventResponse, EmittedResource,
+        emitted_resource::Resource,
+        internal_event_emitter_service_client::InternalEventEmitterServiceClient,
+        CollectionResource, EmitEventRequest, EmitEventResponse, EmittedResource, ObjectResource,
+        ProjectResource,
     },
     notification::services::v1::EventType,
     storage::models::v1::ResourceType,
 };
 use tonic::metadata::{AsciiMetadataKey, AsciiMetadataValue};
 
-use crate::error::ArunaError;
+use crate::{database::models::object::Relation, error::ArunaError};
 
 // Create a client interceptor which always adds the specified API token to the request header
 #[derive(Clone)]
@@ -89,6 +91,86 @@ impl NotificationEmitClient {
             resource_id,
             event_type: event_type as i32,
             resources: emitted_resource,
+        })
+        .await?
+        .into_inner();
+
+        Ok(response)
+    }
+
+    /// Tries to connect to an internal event emitter service to emit the
+    /// event notification.
+    ///
+    /// ## Arguments:
+    ///
+    /// * `resource_id` - Unique id of the resource
+    /// * `resource_type` - Type of the resource
+    /// * `event_type` - Action that was performed with the resource
+    /// * `relation` - Contains the hierarchy relation information of the resource
+    ///
+    /// ## Returns::
+    ///
+    /// * `Result<(), ArunaError>` - An empty Ok result signals success; Error else.
+    ///
+    pub async fn emit_event_with_relation(
+        &self,
+        resource_id: &str,
+        resource_type: ResourceType,
+        event_type: EventType,
+        resource_relation: &Relation,
+    ) -> Result<EmitEventResponse, ArunaError> {
+        let response = InternalEventEmitterServiceClient::with_interceptor(
+            self.emitter_endpoint
+                .clone()
+                .connect()
+                .await
+                .map_err(|err| {
+                    ArunaError::ConnectionError(
+                        crate::error::ConnectionError::TonicConnectionError(err),
+                    )
+                })?,
+            self.interceptor.clone(),
+        )
+        .emit_event(EmitEventRequest {
+            resource_id: resource_id.to_string(),
+            event_resource: resource_type as i32,
+            event_type: event_type as i32,
+            resources: match resource_type {
+                ResourceType::Unspecified => {
+                    return Err(ArunaError::InvalidRequest(
+                        "Unspecified resource type not allowed.".to_string(),
+                    ))
+                }
+                ResourceType::Project => vec![EmittedResource {
+                    resource: Some(Resource::Project(ProjectResource {
+                        project_id: resource_relation.project_id.to_string(),
+                    })),
+                }],
+                ResourceType::Collection => vec![EmittedResource {
+                    resource: Some(Resource::Collection(CollectionResource {
+                        project_id: resource_relation.project_id.to_string(),
+                        collection_id: resource_relation.collection_id.to_string(),
+                    })),
+                }],
+                ResourceType::ObjectGroup => {
+                    return Err(ArunaError::InvalidRequest(
+                        "ObjectGroups are deprecated.".to_string(),
+                    ))
+                }
+                ResourceType::Object => vec![EmittedResource {
+                    resource: Some(Resource::Object(ObjectResource {
+                        project_id: resource_relation.project_id.to_string(),
+                        collection_id: resource_relation.collection_id.to_string(),
+                        shared_object_id: resource_relation.shared_revision_id.to_string(),
+                        object_id: resource_relation.object_id.to_string(),
+                    })),
+                }],
+                ResourceType::All => {
+                    return Err(ArunaError::InvalidRequest(
+                        "Resource type ALL not yet implemented.".to_string(),
+                    ))
+                }
+            },
         })
         .await?
         .into_inner();
