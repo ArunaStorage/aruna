2b84b9e2b9f84146f0d2d78dc72ea982770b889b
St4NNi <sb@pus.de>
2023-07-11 11:41:40 +0200

    feat: Added main to initialize database for tests

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,57 +1,98 @@
-DROP DATABASE IF EXISTS test;
-DROP TYPE IF EXISTS object_status, key_value_type, dataclass, user_rights, object_type, hash_type, endpoint_status  CASCADE;
-CREATE DATABASE test;
-\c test;
 /* ----- Type ENUMs ------------------------------------------------ */
 -- All ENUM types have to be created before their usage in a table
-CREATE TYPE "ObjectStatus" AS ENUM (
-    'INITIALIZING',
-    'VALIDATING',
-    'AVAILABLE',
-    'ERROR',
-    'DELETED'
-);
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'ObjectStatus') THEN
+        CREATE TYPE "ObjectStatus" AS ENUM (
+            'INITIALIZING',
+            'VALIDATING',
+            'AVAILABLE',
+            'ERROR',
+            'DELETED'
+        );
+    END IF;
+END
+$$;
 
-CREATE TYPE "DataClass" AS ENUM ('PUBLIC', 'PRIVATE', 'CONFIDENTIAL');
-CREATE TYPE "KeyValueType" AS ENUM ('LABEL', 'HOOK');
-CREATE TYPE "UserRights" AS ENUM ('READ', 'APPEND', 'CASCADING', 'WRITE', 'ADMIN');
-CREATE TYPE "ObjectType" AS ENUM (
-    'PROJECT',
-    'COLLECTION',
-    'DATASET',
-    'OBJECT'
-);
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'DataClass') THEN
+        CREATE TYPE "DataClass" AS ENUM ('PUBLIC', 'PRIVATE', 'CONFIDENTIAL');
+    END IF;
+END
+$$;
 
-CREATE TYPE "HashType" AS ENUM (
-    'MD5',
-    'SHA256'
-);
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'KeyValueType') THEN
+        CREATE TYPE "KeyValueType" AS ENUM ('LABEL', 'HOOK');
+    END IF;
+END
+$$;
 
-CREATE TYPE "EndpointStatus" AS ENUM (
-    'INITIALIZING',
-    'AVAILABLE',
-    'DEGRADED',
-    'UNAVAILABLE',
-    'MAINTENANCE'
-);
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'UserRights') THEN
+        CREATE TYPE "UserRights" AS ENUM ('READ', 'APPEND', 'CASCADING', 'WRITE', 'ADMIN');
+    END IF;
+END
+$$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'ObjectType') THEN
+        CREATE TYPE "ObjectType" AS ENUM (
+            'PROJECT',
+            'COLLECTION',
+            'DATASET',
+            'OBJECT'
+        );
+    END IF;
+END
+$$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'HashType') THEN
+        CREATE TYPE "HashType" AS ENUM (
+            'MD5',
+            'SHA256'
+        );
+    END IF;
+END
+$$;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'EndpointStatus') THEN
+        CREATE TYPE "EndpointStatus" AS ENUM (
+            'INITIALIZING',
+            'AVAILABLE',
+            'DEGRADED',
+            'UNAVAILABLE',
+            'MAINTENANCE'
+        );
+    END IF;
+END
+$$;
 
 /* ----- Authorization --------------------------------------------- */
 -- Table with users imported from some aai
 -- Join table to map users to multiple identity providers
-CREATE TABLE identity_providers (
+CREATE TABLE IF NOT EXISTS identity_providers (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
     url TEXT NOT NULL
 );
 
-CREATE TABLE external_user_ids (
+CREATE TABLE IF NOT EXISTS external_user_ids (
     id UUID PRIMARY KEY,
     external_id VARCHAR(511) NOT NULL,
     idp_id UUID NOT NULL,
     FOREIGN KEY (idp_id) REFERENCES identity_providers(id)
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
     external_idp_id UUID[],
     display_name TEXT NOT NULL DEFAULT '',
@@ -61,7 +102,7 @@ CREATE TABLE users (
 
 /* ----- Object Service -------------------------------------------- */
 -- Table with objects which represent individual data blobs
-CREATE TABLE objects (
+CREATE TABLE IF NOT EXISTS objects (
     id UUID NOT NULL PRIMARY KEY, -- The unique per object id
     shared_id UUID NOT NULL,             -- A shared ID for all updated versions
     revision_number INT NOT NULL,
@@ -77,12 +118,12 @@ CREATE TABLE objects (
     hashes TEXT[] NOT NULL,
     UNIQUE(shared_id, revision_number)
 );
-CREATE INDEX objects_shared_rev_idx ON objects (shared_id, revision_number);
-CREATE INDEX objects_shared_single_idx ON objects (shared_id);
-CREATE INDEX objects_pk_idx ON objects (id);
+CREATE INDEX IF NOT EXISTS objects_shared_rev_idx ON objects (shared_id, revision_number);
+CREATE INDEX IF NOT EXISTS objects_shared_single_idx ON objects (shared_id);
+CREATE INDEX IF NOT EXISTS objects_pk_idx ON objects (id);
 
 -- Table with endpoints
-CREATE TABLE endpoints (
+CREATE TABLE IF NOT EXISTS endpoints (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
     host_config JSONB NOT NULL,
@@ -93,7 +134,7 @@ CREATE TABLE endpoints (
 );
 
 -- Table with object locations which describe
-CREATE TABLE object_locations (
+CREATE TABLE IF NOT EXISTS object_locations (
     id UUID PRIMARY KEY,
     endpoint_id UUID NOT NULL REFERENCES endpoints(id),
     object_id UUID NOT NULL,
@@ -104,13 +145,13 @@ CREATE TABLE object_locations (
 );
 /* ----- Object Relations ------------------------------------------ */
 -- Table to store custom relation types
-CREATE TABLE relation_types (
+CREATE TABLE IF NOT EXISTS relation_types (
     id SMALLSERIAL PRIMARY KEY NOT NULL,
     relation_name VARCHAR(511) NOT NULL 
 );
 
 -- Table to store all internal relations between objects
-CREATE TABLE internal_relations (
+CREATE TABLE IF NOT EXISTS internal_relations (
     id UUID PRIMARY KEY NOT NULL,
     origin_pid UUID REFERENCES objects(id) ON DELETE CASCADE,
     origin_did UUID, -- Cannot be a foreign key
@@ -120,7 +161,7 @@ CREATE TABLE internal_relations (
 );
 
 -- Table with api tokens which are used to authorize user actions in a specific project and/or collection
-CREATE TABLE api_tokens (
+CREATE TABLE IF NOT EXISTS api_tokens (
     id UUID PRIMARY KEY NOT NULL,
     user_id UUID NOT NULL,
     pub_key SERIAL NOT NULL,
@@ -135,14 +176,14 @@ CREATE TABLE api_tokens (
 );
 
 -- Table for available pubkeys
-CREATE TABLE pub_keys (
+CREATE TABLE IF NOT EXISTS pub_keys (
     id INT PRIMARY KEY, -- This is a serial to make jwt tokens smaller
     pubkey TEXT NOT NULL
 );
 
 /* ----- Notification Service -------------------------------------- */
 -- Table for the notification service to persist consumer
-CREATE TABLE notification_stream_groups (
+CREATE TABLE IF NOT EXISTS notification_stream_groups (
     id UUID PRIMARY KEY,
     resource_id UUID NOT NULL,
     subject TEXT NOT NULL,
