d14b294928f77734b017e3e58236e8fc5f21f7a9
St4NNi <sb@pus.de>
2023-07-11 11:04:51 +0200

    feat: Added object json add/remove functions updates

--- src/database/object_dsl.rs
+++ src/database/object_dsl.rs
@@ -2,11 +2,47 @@ use crate::database::{
     crud::{CrudDb, PrimaryKey},
     enums::{DataClass, ObjectStatus, ObjectType},
 };
+
+use anyhow::anyhow;
 use anyhow::Result;
 use chrono::NaiveDateTime;
 use diesel_ulid::DieselUlid;
 use postgres_from_row::FromRow;
+use postgres_types::Json;
+use serde::{Deserialize, Serialize};
 use tokio_postgres::Client;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
+pub enum KeyValueVariant {
+    HOOK,
+    LABEL,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
+pub struct KeyValue {
+    pub key: String,
+    pub value: String,
+    pub variant: KeyValueVariant,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
+pub struct KeyValues(pub Vec<KeyValue>);
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
+pub enum RelationVariant {
+    URL,
+    IDENTIFIER,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
+pub struct ExternalRelation {
+    pub name: String,
+    pub identifier: String,
+    pub variant: RelationVariant,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
+pub struct ExternalRelations(pub Vec<ExternalRelation>);
 
 #[derive(FromRow, Debug)]
 pub struct Object {
@@ -17,11 +53,11 @@ pub struct Object {
     pub created_at: Option<NaiveDateTime>,
     pub created_by: DieselUlid,
     pub content_len: i64,
-    pub key_values: serde_json::Value,
+    pub key_values: Json<KeyValues>,
     pub object_status: ObjectStatus,
     pub data_class: DataClass,
     pub object_type: ObjectType,
-    pub external_relations: serde_json::Value,
+    pub external_relations: Json<ExternalRelations>,
     pub hashes: Vec<String>,
 }
 
@@ -74,6 +110,72 @@ impl CrudDb for Object {
         let query = "DELETE FROM objects WHERE id = $1";
         let prepared = client.prepare(query).await?;
         client.execute(&prepared, &[&id]).await?;
+        Ok(())
+    }
+}
+
+impl Object {
+    pub async fn add_key_value(id: &DieselUlid, client: &Client, kv: KeyValue) -> Result<()> {
+        let query = "UPDATE objects
+        SET key_values = key_values || $1::jsonb
+        WHERE id = $2;";
+
+        let prepared = client.prepare(query).await?;
+        client.execute(&prepared, &[&Json(kv), id]).await?;
+        Ok(())
+    }
+
+    pub async fn remove_key_value(&self, client: &Client, kv: KeyValue) -> Result<()> {
+        let element: i32 = self
+            .key_values
+            .0
+             .0
+            .iter()
+            .position(|e| *e == kv)
+            .ok_or_else(|| anyhow!("Unable to find key_value"))? as i32;
+
+        let query = "UPDATE objects
+        SET key_values = key_values - $1::INTEGER
+        WHERE id = $2;";
+
+        let prepared = client.prepare(query).await?;
+        client.execute(&prepared, &[&element, &self.id]).await?;
+        Ok(())
+    }
+
+    pub async fn add_external_relations(
+        id: &DieselUlid,
+        client: &Client,
+        rel: ExternalRelation,
+    ) -> Result<()> {
+        let query = "UPDATE objects
+        SET external_relations = external_relations || $1::jsonb
+        WHERE id = $2;";
+
+        let prepared = client.prepare(query).await?;
+        client.execute(&prepared, &[&Json(rel), id]).await?;
+        Ok(())
+    }
+
+    pub async fn remove_external_relation(
+        &self,
+        client: &Client,
+        rel: ExternalRelation,
+    ) -> Result<()> {
+        let element: i32 = self
+            .external_relations
+            .0
+             .0
+            .iter()
+            .position(|e| *e == rel)
+            .ok_or_else(|| anyhow!("Unable to find key_value"))? as i32;
+
+        let query = "UPDATE objects
+        SET external_relations = external_relations - $1::INTEGER
+        WHERE id = $2;";
+
+        let prepared = client.prepare(query).await?;
+        client.execute(&prepared, &[&element, &self.id]).await?;
         Ok(())
     }
 }
