46150739ed955e2365b46ff5b9123c45f2e3c110
St4NNi <sb@pus.de>
2023-07-11 11:53:48 +0200

    feat: Added basic main initializing functions

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
-use deadpool_postgres::{Config, GenericClient, ManagerConfig, RecyclingMethod, Runtime};
-use tokio_postgres::NoTls;
+use anyhow::Result;
+use aruna_server::database;
 
 #[tokio::main]
-pub async fn main() {
+pub async fn main() -> Result<()> {
     let database_host = "localhost";
     let database_name = "test";
     let database_port = 5433;
     let database_user = "yugabyte";
 
-    let mut cfg = Config::new();
-    cfg.host = Some(database_host.to_string());
-    cfg.port = Some(database_port);
-    cfg.user = Some(database_user.to_string());
-    cfg.dbname = Some(database_name.to_string());
-    cfg.manager = Some(ManagerConfig {
-        recycling_method: RecyclingMethod::Fast,
-    });
-    let pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls).unwrap();
-    let client = pool.get().await.unwrap();
-    let _ = client
-        .execute("CREATE DATABASE $1", &[&database_name])
-        .await;
-    let initial = tokio::fs::read_to_string("./src/database/schema.sql")
-        .await
-        .unwrap();
-    client.batch_execute(&initial).await.unwrap();
+    let db = database::connection::Database::new(
+        database_host,
+        database_port,
+        database_name,
+        database_user,
+    );
+    db.initialize_db().await?;
+    Ok(())
 }
