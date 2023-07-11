ef21ba5f6461e28071d5e5a51ca7c9b93ded1b6f
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2023-07-11 10:55:07 +0200

    feat: Add notification_dsl to database crud implementation

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -2,6 +2,7 @@ pub mod api_tokens_dsl;
 pub mod crud;
 pub mod enums;
 pub mod internal_relations_dsl;
+pub mod notification_dsl;
 pub mod object_dsl;
 pub mod pub_keys_dsl;
 pub mod relation_types_dsl;
