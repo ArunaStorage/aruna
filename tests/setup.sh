e80f2514b6af9e2f6c5f2898e9e5029d0534bead
St4NNi <sb@pus.de>
2023-07-11 13:19:04 +0200

    tests: Added checks for yugabyte startup

--- tests/setup.sh
+++ tests/setup.sh
@@ -2,4 +2,12 @@
 apt-get update
 apt-get install --yes --no-install-recommends postgresql-client 
 docker run -d --name yugabyte -p5433:5433 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false
+
+until [ "docker inspect -f {{.State.Running}} yugabyte"=="true" ]; do
+    sleep 1;
+done;
+# Give the container some time to be available
+sleep 5;
+
+
 psql "postgres://yugabyte@localhost:5433" -c 'CREATE DATABASE test' 
