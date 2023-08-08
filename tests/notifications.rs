314caec4eba036a3a1ed8890350ba7e02fe9d3b4
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2023-08-08 21:59:55 +0200

    tests: Add basic notification emit test

--- tests/notifications.rs
+++ tests/notifications.rs
@@ -125,3 +125,118 @@ async fn delete_stream_consumer() {
         .unwrap()
         .is_none());
 }
+
+#[tokio::test]
+async fn notification_test() {
+    // Init Nats.io connection
+    let nats_client = async_nats::connect("0.0.0.0:4222").await.unwrap();
+    let nats_handler = NatsIoHandler::new(nats_client, "ThisIsASecretToken".to_string(), None)
+        .await
+        .unwrap();
+
+    // Init database connection
+    let db = common::init_db::init_db().await;
+    let client = db.get_client().await.unwrap();
+
+    // Create random user
+    let user = common::test_utils::new_user(vec![]);
+    let random_user_id = user.id.clone();
+    user.create(&client).await.unwrap();
+
+    // Create dummy hierarchy
+    let project_001 =
+        common::test_utils::new_object(random_user_id, DieselUlid::generate(), ObjectType::PROJECT);
+    let project_002 =
+        common::test_utils::new_object(random_user_id, DieselUlid::generate(), ObjectType::PROJECT);
+    let project_003 =
+        common::test_utils::new_object(random_user_id, DieselUlid::generate(), ObjectType::PROJECT);
+    let collection = common::test_utils::new_object(
+        random_user_id,
+        DieselUlid::generate(),
+        ObjectType::COLLECTION,
+    );
+    let dataset_001 =
+        common::test_utils::new_object(random_user_id, DieselUlid::generate(), ObjectType::DATASET);
+    let dataset_002 =
+        common::test_utils::new_object(random_user_id, DieselUlid::generate(), ObjectType::DATASET);
+    let object =
+        common::test_utils::new_object(random_user_id, DieselUlid::generate(), ObjectType::OBJECT);
+
+    let proj_coll_001 = common::test_utils::new_internal_relation(&project_001, &collection);
+    let proj_coll_002 = common::test_utils::new_internal_relation(&project_002, &collection);
+    let proj_data = common::test_utils::new_internal_relation(&project_003, &dataset_002);
+    let coll_data_001 = common::test_utils::new_internal_relation(&collection, &dataset_001);
+    let coll_data_002 = common::test_utils::new_internal_relation(&collection, &dataset_002);
+    let data_obj_001 = common::test_utils::new_internal_relation(&dataset_001, &object);
+    let data_obj_002 = common::test_utils::new_internal_relation(&dataset_002, &object);
+
+    Object::batch_create(
+        &vec![
+            project_001.clone(),
+            project_002,
+            project_003.clone(),
+            collection,
+            dataset_001,
+            dataset_002,
+            object.clone(),
+        ],
+        &client,
+    )
+    .await
+    .unwrap();
+
+    InternalRelation::batch_create(
+        &vec![
+            proj_coll_001,
+            proj_coll_002,
+            proj_data,
+            coll_data_001,
+            coll_data_002,
+            data_obj_001,
+            data_obj_002,
+        ],
+        &client,
+    )
+    .await
+    .unwrap();
+
+    // Create stream consumer in Nats.io for one of the projects
+    let (stream_consumer_id, _) = nats_handler
+        .create_event_consumer(
+            EventType::Resource((project_003.id.to_string(), ObjectType::PROJECT, true)),
+            DeliverPolicy::All,
+        )
+        .await
+        .unwrap();
+
+    // Send notification for object creation
+    let object_plus = Object::get_object_with_relations(&object.id, &client)
+        .await
+        .unwrap();
+
+    // Warm up database query layer
+    object.fetch_object_hierarchies(&client).await.unwrap();
+
+    // Performance time.
+    use std::time::Instant;
+    let now = Instant::now();
+    let object_hierarchies = object.fetch_object_hierarchies(&client).await.unwrap();
+    let elapsed = now.elapsed();
+    println!("Path fetch: {:.2?}", elapsed);
+
+    let now = Instant::now();
+    nats_handler
+        .register_resource_event(&object_plus, object_hierarchies, EventVariant::Created)
+        .await
+        .unwrap();
+    let elapsed = now.elapsed();
+    println!("Notification emit: {:.2?}", elapsed);
+
+    // Evaluate number of messages received
+    let nats_messages = nats_handler
+        .get_event_consumer_messages(stream_consumer_id.to_string(), 10)
+        .await
+        .unwrap();
+
+    assert_eq!(nats_messages.len(), 2);
+}
