b58eb22d08cb032d6d7d4f924c4aee0c35bf73f4
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2023-08-10 11:36:16 +0200

    feat: Add function to query pubkey by key

--- src/database/dsls/pub_key_dsl.rs
+++ src/database/dsls/pub_key_dsl.rs
@@ -45,3 +45,17 @@ impl CrudDb for PubKey {
         Ok(())
     }
 }
+
+impl PubKey {
+    /// Queries the pub_keys table for a specific pubkey.
+    /// As the pubkeys are unique on the database level it returns an Option which includes the
+    /// pubkey if it exists.
+    pub async fn get_by_key(&self, pubkey: &str, client: &Client) -> Result<Option<PubKey>> {
+        let query = "SELECT * FROM pub_keys WHERE pubkey = $1";
+        let prepared = client.prepare(query).await?;
+        Ok(client
+            .query_opt(&prepared, &[&pubkey])
+            .await?
+            .map(|e| PubKey::from_row(&e)))
+    }
+}
