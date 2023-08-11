b1569466cc51cf86dc44da801466d4b74a5f583e
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2023-08-11 10:31:28 +0200

    refactor: PermissionHandler init

--- src/auth/permission_handler.rs
+++ src/auth/permission_handler.rs
@@ -5,14 +5,14 @@ use std::sync::Arc;
 
 pub struct PermissionHandler {
     cache: Arc<Cache>,
-    token_handler: Arc<TokenHandler>,
+    pub token_handler: Arc<TokenHandler>,
 }
 
 impl PermissionHandler {
-    pub fn new(cache: Arc<Cache>, realm_info: &str) -> Self {
+    pub fn new(cache: Arc<Cache>, token_handler: Arc<TokenHandler>) -> Self {
         Self {
             cache: cache.clone(),
-            token_handler: Arc::new(TokenHandler::new(cache, realm_info.to_string())),
+            token_handler: token_handler, //Arc::new(TokenHandler::new(cache, realm_info.to_string())),
         }
     }
 
