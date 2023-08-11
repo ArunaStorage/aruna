823bae28573f3f16f7778c06db045b8303b12c6f
lfbrehm <97600985+lfbrehm@users.noreply.github.com>
2023-08-11 10:08:22 +0200

    feat: Added and snapshot dataset test.

--- tests/middlelayer/snapshots.rs
+++ tests/middlelayer/snapshots.rs
@@ -1,5 +1,5 @@
 use crate::common::{init_db::init_handler, test_utils};
-use aruna_rust_api::api::storage::services::v2::ArchiveProjectRequest;
+use aruna_rust_api::api::storage::services::v2::{ArchiveProjectRequest, SnapshotDatasetRequest};
 use aruna_server::database::crud::CrudDb;
 use aruna_server::database::dsls::internal_relation_dsl::InternalRelation;
 use aruna_server::database::dsls::object_dsl::Object;
@@ -59,4 +59,41 @@ async fn test_archive() {
     });
     let (_, archive) = db_handler.snapshot(request).await.unwrap();
     assert!(archive.iter().all(|o| !o.object.dynamic));
+}
+
+#[tokio::test]
+async fn test_snapshot_dataset() {
+    // Init
+    let db_handler = init_handler().await;
+    let dataset_id = DieselUlid::generate();
+    let o1_id = DieselUlid::generate();
+    let o2_id = DieselUlid::generate();
+    let user = test_utils::new_user(vec![
+        ObjectMapping::DATASET(dataset_id),
+        ObjectMapping::OBJECT(o1_id),
+        ObjectMapping::OBJECT(o2_id),
+    ]);
+    let mut dataset = test_utils::new_object(user.id, dataset_id, ObjectType::DATASET);
+    dataset.dynamic = true;
+    let object_1 = test_utils::new_object(user.id, o1_id, ObjectType::OBJECT);
+    let object_2 = test_utils::new_object(user.id, o2_id, ObjectType::OBJECT);
+    let d_o1 = test_utils::new_internal_relation(&dataset, &object_1);
+    let d_o2 = test_utils::new_internal_relation(&dataset, &object_2);
+    let rels = vec![d_o1, d_o2];
+    let objects = vec![dataset, object_1, object_2];
+    let client = db_handler.database.get_client().await.unwrap();
+    user.create(&client).await.unwrap();
+    Object::batch_create(&objects, &client).await.unwrap();
+    InternalRelation::batch_create(&rels, &client)
+        .await
+        .unwrap();
+
+    // test
+    let request = SnapshotRequest::Dataset(SnapshotDatasetRequest {
+        dataset_id: dataset_id.to_string(),
+    });
+    let (_, snapshot) = db_handler.snapshot(request).await.unwrap();
+    assert!(snapshot.iter().all(|o| !o.object.dynamic));
+    let old = Object::get(dataset_id, &client).await.unwrap().unwrap();
+    assert!(old.dynamic);
 }
