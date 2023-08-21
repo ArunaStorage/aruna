8cbd662d6549bf088a9cf06dc50aa57e407b3ccc
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2023-08-21 20:47:16 +0200

    feat: Add notifications to clone db handler

--- src/middlelayer/clone_db_handler.rs
+++ src/middlelayer/clone_db_handler.rs
@@ -6,9 +6,12 @@ use crate::database::dsls::object_dsl::Object;
 use crate::database::dsls::object_dsl::ObjectWithRelations;
 use crate::database::enums::{ObjectMapping, ObjectType};
 use crate::middlelayer::db_handler::DatabaseHandler;
+
 use anyhow::{anyhow, Result};
+use aruna_rust_api::api::notification::services::v2::EventVariant;
 use deadpool_postgres::GenericClient;
 use diesel_ulid::DieselUlid;
+use std::collections::HashMap;
 
 impl DatabaseHandler {
     pub async fn clone_object(
@@ -17,12 +20,18 @@ impl DatabaseHandler {
         object_id: &DieselUlid,
         parent: ObjectMapping<DieselUlid>,
     ) -> Result<ObjectWithRelations> {
+        // Get standard database client
         let client = self.database.get_client().await?;
-        let object = Object::get_object_with_relations(object_id, &client).await?;
+
+        // Fetch object to be cloned
+        let original_object = Object::get_object_with_relations(object_id, &client).await?;
+
+        // Prepare cloned object
         let new_id = DieselUlid::generate();
-        let mut clone = object.object;
+        let mut clone = original_object.object;
         clone.id = new_id;
         clone.created_by = *user_id;
+
         let (origin_pid, origin_type) = match parent {
             ObjectMapping::PROJECT(id) => (id, ObjectType::PROJECT),
             ObjectMapping::COLLECTION(id) => (id, ObjectType::COLLECTION),
@@ -37,12 +46,41 @@ impl DatabaseHandler {
             target_pid: new_id,
             target_type: ObjectType::OBJECT,
         };
-        let mut t_client = self.database.get_client().await?;
-        let transaction = t_client.transaction().await?;
+
+        // Create object and relation in transaction
+        let mut db_client = self.database.get_client().await?;
+        let transaction = db_client.transaction().await?;
         let transaction_client = transaction.client();
         clone.create(transaction_client).await?;
         relation.create(transaction_client).await?;
         transaction.commit().await?;
-        Object::get_object_with_relations(&new_id, &client).await
+
+        // Fetch object DTOs for cloned object and parent
+        let mut objects_plus: HashMap<DieselUlid, ObjectWithRelations> =
+            Object::get_objects_with_relations(&vec![new_id, origin_pid], &client)
+                .await?
+                .into_iter()
+                .map(|object_plus| (object_plus.object.id, object_plus))
+                .collect();
+
+        // Fetch all object paths for the notification subjects
+        for (_, object_plus) in &objects_plus {
+            let object_hierarchies = clone.fetch_object_hierarchies(&client).await?;
+
+            // Try to emit object created notification(s)
+            if let Err(err) = self
+                .natsio_handler
+                .register_resource_event(object_plus, object_hierarchies, EventVariant::Created)
+                .await
+            {
+                // Log and return error
+                log::error!("{}", err);
+                return Err(anyhow::anyhow!("Notification emission failed"));
+            }
+        }
+
+        Ok(objects_plus
+            .remove(&new_id)
+            .ok_or_else(|| anyhow!("Object disappeared into the void"))?)
     }
 }
