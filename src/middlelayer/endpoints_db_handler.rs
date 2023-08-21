e788b71bdf9a70834e82760e402b2588db2b9485
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2023-08-21 11:58:44 +0200

    feat: Add notifications to endpoint db handler

--- src/middlelayer/endpoints_db_handler.rs
+++ src/middlelayer/endpoints_db_handler.rs
@@ -3,7 +3,9 @@ use crate::database::dsls::endpoint_dsl::Endpoint;
 use crate::database::dsls::pub_key_dsl::PubKey;
 use crate::middlelayer::db_handler::DatabaseHandler;
 use crate::middlelayer::endpoints_request_types::{CreateEP, DeleteEP, GetBy, GetEP};
+
 use anyhow::{anyhow, Result};
+use aruna_rust_api::api::notification::services::v2::anouncement_event::EventVariant as AnnouncementVariant;
 use tokio_postgres::GenericClient;
 
 impl DatabaseHandler {
@@ -16,9 +18,24 @@ impl DatabaseHandler {
         pubkey.id = idx + 1;
         endpoint.create(transaction_client).await?;
         pubkey.create(transaction_client).await?;
+
+        //ToDo: Notifications
+        //  - Endpoint created
+        //  - PubKey created
+        let announcements = vec![
+            AnnouncementVariant::NewDataProxyId(endpoint.id.to_string()),
+            AnnouncementVariant::NewPubkey(true),
+        ];
+
+        // Emit announcement notifications
+        for ann in announcements {
+            self.natsio_handler.register_announcement_event(ann).await?;
+        }
+
         transaction.commit().await?;
         Ok((endpoint, pubkey))
     }
+
     pub async fn get_endpoint(&self, request: GetEP) -> Result<Endpoint> {
         let client = self.database.get_client().await?;
         let endpoint = match request.get_query()? {
@@ -27,15 +44,23 @@ impl DatabaseHandler {
         };
         endpoint.ok_or_else(|| anyhow!("No endpoint found"))
     }
+
     pub async fn get_endpoints(&self) -> Result<Vec<Endpoint>> {
         let client = self.database.get_client().await?;
         let endpoints = Endpoint::all(client.client()).await?;
         Ok(endpoints)
     }
+    
     pub async fn delete_endpoint(&self, request: DeleteEP) -> Result<()> {
         let client = self.database.get_client().await?;
         let id = request.get_id()?;
         Endpoint::delete_by_id(&id, client.client()).await?;
+
+        // Emit announcement notification
+        self.natsio_handler
+            .register_announcement_event(AnnouncementVariant::RemoveDataProxyId(id.to_string()))
+            .await?;
+
         Ok(())
     }
 }
