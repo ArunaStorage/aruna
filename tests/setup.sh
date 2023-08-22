ca9656c45c4231ad94b909a5f027f72bd19f39ed
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2023-08-22 11:25:19 +0200

    feat: Added yugabyte tserver flags for concurrency control

--- tests/setup.sh
+++ tests/setup.sh
@@ -17,7 +17,8 @@ $Runtime run -d --name nats-js-server --rm -p 4222:4222 -p 8222:8222 nats:latest
 $Runtime run -d --rm -p 7700:7700 -e MEILI_MASTER_KEY='MASTER_KEY' getmeili/meilisearch:latest
 
 # Start yugabyte container
-$Runtime run -d --name yugabyte -p5433:5433 yugabytedb/yugabyte:latest bin/yugabyted start --daemon=false
+$Runtime run -d --name yugabyte -p5433:5433 yugabytedb/yugabyte:latest bin/yugabyted start\
+ --tserver_flags="enable_wait_queues=true,enable_deadlock_detection=true,yb_enable_read_committed_isolation=true" --daemon=false
 
 until [ "${Runtime} inspect -f {{.State.Running}} yugabyte"=="true" ]; do
     echo "Waiting for container startup ..."
@@ -41,5 +42,5 @@ psql "postgres://yugabyte@localhost:5433/test" < $(pwd)/src/database/schema.sql
 # Add initial data (script has to be called from project root)
 psql "postgres://yugabyte@localhost:5433/test" < $(pwd)/tests/common/initial_data.sql
 
-
+# Start something that gives an emulated keycloak response 
 $Runtime run -d --name fake-keycloak -p 8999:80 -v "$(pwd)/tests/common/keycloak/:/usr/local/apache2/htdocs/" httpd:2.4
