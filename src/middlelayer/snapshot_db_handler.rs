0e9de0abcc90766c48b47b518be4a92c63430a2d
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2023-08-23 10:57:29 +0200

    feat: Add archive/snapshot notifications

--- src/middlelayer/snapshot_db_handler.rs
+++ src/middlelayer/snapshot_db_handler.rs
@@ -2,6 +2,7 @@ use crate::database::dsls::object_dsl::{Object, ObjectWithRelations};
 use crate::middlelayer::db_handler::DatabaseHandler;
 use crate::middlelayer::snapshot_request_types::{SnapshotRequest, SnapshotResponse};
 use anyhow::Result;
+use aruna_rust_api::api::notification::services::v2::EventVariant;
 use diesel_ulid::DieselUlid;
 
 impl DatabaseHandler {
@@ -12,29 +13,99 @@ impl DatabaseHandler {
         let client = self.database.get_client().await?;
         let id = request.get_id()?;
         let resource = Object::get_object_with_relations(&id, &client).await?;
+
+        // Evaluate snapshot type
         let (new_object_id, mut snapshot_resources) = match request {
             SnapshotRequest::Project(_) => (
                 id,
                 SnapshotResponse::ArchiveProject(
-                    SnapshotRequest::get_archived_project(resource, &client).await?,
+                    SnapshotRequest::get_archived_project(&resource, &client).await?,
                 ),
             ),
             SnapshotRequest::Collection(_) => {
-                let request = SnapshotRequest::get_cloned_collection(resource, &client).await?;
+                let request =
+                    SnapshotRequest::get_cloned_collection(resource.clone(), &client).await?;
                 (
                     request.collection.id,
                     SnapshotResponse::SnapshotCollection(request),
                 )
             }
             SnapshotRequest::Dataset(_) => {
-                let request = SnapshotRequest::get_cloned_dataset(resource).await?;
+                let request = SnapshotRequest::get_cloned_dataset(resource.clone()).await?;
                 (
                     request.dataset.id,
                     SnapshotResponse::SnapshotDataset(request),
                 )
             }
         };
+
+        // Execute archive/snapshot
         let result = snapshot_resources.snapshot(client).await?;
+
+        // Notifications
+        let client = self.database.get_client().await?; // Refresh database connection ...
+        let emissions = match snapshot_resources {
+            SnapshotResponse::ArchiveProject(_) => {
+                //  - Project archive -> All affected objects EventVariant::Updated
+                let mut notifications = vec![];
+                for obj in &result {
+                    notifications.push((
+                        obj,
+                        obj.object.fetch_object_hierarchies(&client).await?,
+                        EventVariant::Updated,
+                    ))
+                }
+
+                notifications
+            }
+            SnapshotResponse::SnapshotCollection(snapshot) => {
+                //  - Collection snapshot -> Old Object updated, New Object snapshotted
+                vec![
+                    (
+                        &resource,
+                        resource.object.fetch_object_hierarchies(&client).await?,
+                        EventVariant::Updated,
+                    ),
+                    (
+                        &result[0],
+                        snapshot
+                            .collection
+                            .fetch_object_hierarchies(&client)
+                            .await?,
+                        EventVariant::Snapshotted,
+                    ),
+                ]
+            }
+            SnapshotResponse::SnapshotDataset(snapshot) => {
+                //  - Dataset snapshot    -> Old Object updated, New Object snapshotted
+                vec![
+                    (
+                        &resource,
+                        resource.object.fetch_object_hierarchies(&client).await?,
+                        EventVariant::Updated,
+                    ),
+                    (
+                        &result[0],
+                        snapshot.dataset.fetch_object_hierarchies(&client).await?,
+                        EventVariant::Snapshotted,
+                    ),
+                ]
+            }
+        };
+
+        for (object_plus, hierarchies, event_variant) in emissions {
+            if let Err(err) = self
+                .natsio_handler
+                .register_resource_event(object_plus, hierarchies, event_variant)
+                .await
+            {
+                // Log error, rollback transaction and return
+                log::error!("{}", err);
+                //transaction.rollback().await?;
+                return Err(anyhow::anyhow!("Notification emission failed"));
+            }
+        }
+
         Ok((new_object_id, result))
     }
 }
