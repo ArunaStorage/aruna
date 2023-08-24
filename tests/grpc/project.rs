395d8b496c3de01232993875098be31a145774b8
Jannis Hochmuth <Jannis.Hochmuth@computational.bio.uni-giessen.de>
2023-08-24 17:55:59 +0200

    test: Add test for gRPC Project get

--- tests/grpc/project.rs
+++ tests/grpc/project.rs
@@ -1,51 +1,37 @@
 use aruna_rust_api::api::storage::{
-    models::v2::{DataEndpoint, Status},
-    services::v2::{project_service_server::ProjectService, CreateProjectRequest},
+    models::v2::{DataClass, DataEndpoint, Status},
+    services::v2::{
+        project_service_server::ProjectService, CreateProjectRequest, GetProjectRequest,
+        GetProjectsRequest,
+    },
 };
-use aruna_server::grpc::projects::ProjectServiceImpl;
-use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use tonic::Request;
 
 use crate::common::{
-    init::{
-        init_cache, init_database, init_database_handler, init_nats_client,
-        init_permission_handler, init_search_client,
+    init::init_project_service,
+    test_utils::{
+        self, add_token, fast_track_grpc_project_create, rand_string, DEFAULT_ENDPOINT_ULID,
+        USER_OIDC_TOKEN,
     },
-    test_utils::{self, add_token, DEFAULT_ENDPOINT_ULID},
 };
 
 #[tokio::test]
 async fn grpc_create_project() {
-    let db = init_database().await;
-    let cache = init_cache(db.clone(), true).await;
-    let natsio_handler = init_nats_client().await;
-    let db_handler = init_database_handler(db.clone(), natsio_handler).await;
-    let search_client = init_search_client().await;
-    let authorizer = init_permission_handler(db.clone(), cache.clone()).await;
-
-    // Init project service
-    let project_service = ProjectServiceImpl::new(
-        db_handler,
-        authorizer,
-        cache,
-        search_client,
-        test_utils::DEFAULT_ENDPOINT_ULID.to_string(),
-    )
-    .await;
+    // Init ProjectService
+    let project_service = init_project_service().await;
 
     // Create request with OIDC token in header
     let project_name = rand_string(32);
 
     let create_request = CreateProjectRequest {
         name: project_name.to_string(),
-        description: "something".to_string(),
+        description: "Something".to_string(),
         key_values: vec![],
         external_relations: vec![],
-        data_class: 1,
+        data_class: DataClass::Public as i32,
         preferred_endpoint: "".to_string(),
     };
 
-    
     let grpc_request = add_token(Request::new(create_request), test_utils::ADMIN_OIDC_TOKEN);
 
     // Create project via gRPC service
@@ -59,7 +45,7 @@ async fn grpc_create_project() {
 
     assert!(!proto_project.id.is_empty());
     assert_eq!(proto_project.name, project_name);
-    assert_eq!(&proto_project.description, "something");
+    assert_eq!(&proto_project.description, "Something");
     assert_eq!(proto_project.key_values, vec![]);
     assert_eq!(proto_project.relations, vec![]);
     assert_eq!(proto_project.data_class, 1);
@@ -73,4 +59,50 @@ async fn grpc_create_project() {
             full_synced: true,
         }]
     );
+}
+
+#[tokio::test]
+async fn grpc_get_projects() {
+    // Init ProjectService
+    let project_service = init_project_service().await;
+
+    // Create multiple projects
+    let project_01 = fast_track_grpc_project_create(&project_service, USER_OIDC_TOKEN).await;
+    let project_02 = fast_track_grpc_project_create(&project_service, USER_OIDC_TOKEN).await;
+
+    // Fetch single project
+    let get_request = GetProjectRequest {
+        project_id: project_01.id.to_string(),
+    };
+
+    let grpc_request = add_token(Request::new(get_request), USER_OIDC_TOKEN);
+
+    let get_response = project_service
+        .get_project(grpc_request)
+        .await
+        .unwrap()
+        .into_inner();
+
+    let proto_project = get_response.project.unwrap();
+
+    assert_eq!(project_01, proto_project);
+
+    // Fetch all projects
+    let get_all_request = GetProjectsRequest {
+        project_ids: vec![project_01.id.to_string(), project_02.id.to_string()],
+    };
+
+    let grpc_request = add_token(Request::new(get_all_request), USER_OIDC_TOKEN);
+
+    let get_all_response = project_service
+        .get_projects(grpc_request)
+        .await
+        .unwrap()
+        .into_inner();
+
+    let proto_projects = get_all_response.projects;
+
+    for project in vec![project_01, project_02] {
+        assert!(proto_projects.contains(&project))
+    }
 }
