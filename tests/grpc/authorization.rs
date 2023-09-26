24ea803bf13c2e5095d03c0434bd3b628c66125c
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2023-09-26 17:03:13 +0200

    test: Add authorization get gRPC test

--- tests/grpc/authorization.rs
+++ tests/grpc/authorization.rs
@@ -4,7 +4,7 @@ use aruna_rust_api::api::storage::{
     models::v2::{permission::ResourceId, Permission, PermissionLevel},
     services::v2::{
         authorization_service_server::AuthorizationService, user_service_server::UserService,
-        CreateAuthorizationRequest, GetUserRequest,
+        CreateAuthorizationRequest, GetAuthorizationsRequest, GetUserRequest, UserPermission,
     },
 };
 use aruna_server::database::{
@@ -16,7 +16,10 @@ use diesel_ulid::DieselUlid;
 
 use crate::common::{
     init::init_service_block,
-    test_utils::{add_token, fast_track_grpc_project_create, ADMIN_OIDC_TOKEN, USER_OIDC_TOKEN},
+    test_utils::{
+        add_token, fast_track_grpc_permission_add, fast_track_grpc_project_create,
+        ADMIN_OIDC_TOKEN, ADMIN_USER_ULID, GENERIC_USER_ULID, USER_OIDC_TOKEN,
+    },
 };
 
 #[tokio::test]
@@ -54,7 +57,7 @@ async fn grpc_create_authorization() {
             resource_id: Some(ResourceId::ProjectId(project.id.clone()))
         }));
 
-    //ToDo: Add permission to user who does not exist
+    // Add permission to user who does not exist
     let mut inner_request = CreateAuthorizationRequest {
         resource_id: project.id.clone(),
         user_id: DieselUlid::generate().to_string(),
@@ -70,7 +73,7 @@ async fn grpc_create_authorization() {
 
     assert!(response.is_err());
 
-    //ToDo: Add permission to user without token
+    // Add permission to user without token
     inner_request.user_id = user.id.clone();
 
     let response = service_block
@@ -80,7 +83,7 @@ async fn grpc_create_authorization() {
 
     assert!(response.is_err());
 
-    //ToDo: Add permission to user without sufficient permission
+    // Add permission to user without sufficient permission
     let grpc_request = add_token(tonic::Request::new(inner_request.clone()), USER_OIDC_TOKEN);
 
     let response = service_block
@@ -90,7 +93,7 @@ async fn grpc_create_authorization() {
 
     assert!(response.is_err());
 
-    //ToDo: Add permission to user
+    // Add permission to user
     let grpc_request = add_token(tonic::Request::new(inner_request.clone()), ADMIN_OIDC_TOKEN);
 
     let response = service_block
@@ -144,7 +147,91 @@ async fn grpc_create_authorization() {
 }
 
 #[tokio::test]
-async fn grpc_get_authorization() {}
+async fn grpc_get_authorization() {
+    // Init gRPC services
+    let service_block = init_service_block().await;
+
+    // Create random project
+    let project =
+        fast_track_grpc_project_create(&service_block.project_service, ADMIN_OIDC_TOKEN).await;
+    let project_ulid = DieselUlid::from_str(&project.id).unwrap();
+
+    // Create ULID from user id
+    let user_ulid = DieselUlid::from_str(GENERIC_USER_ULID).unwrap();
+
+    // Get authorization of resource who does not exist
+    let mut inner_request = GetAuthorizationsRequest {
+        resource_id: DieselUlid::generate().to_string(),
+        recursive: false,
+    };
+
+    let grpc_request = add_token(tonic::Request::new(inner_request.clone()), ADMIN_OIDC_TOKEN);
+
+    let authorizations = service_block
+        .auth_service
+        .get_authorizations(grpc_request)
+        .await
+        .unwrap()
+        .into_inner()
+        .authorizations;
+
+    assert!(authorizations.is_empty());
+
+    // Get authorization of resource without token
+    inner_request.resource_id = project.id;
+
+    let response = service_block
+        .auth_service
+        .get_authorizations(tonic::Request::new(inner_request.clone()))
+        .await;
+
+    assert!(response.is_err());
+
+    // Get authorization of resource without sufficient permissions
+    let grpc_request = add_token(tonic::Request::new(inner_request.clone()), USER_OIDC_TOKEN);
+
+    let response = service_block
+        .auth_service
+        .get_authorizations(grpc_request)
+        .await;
+
+    assert!(response.is_err());
+
+    // Get authorization of resource with sufficient permissions
+    fast_track_grpc_permission_add(
+        &service_block.auth_service,
+        ADMIN_OIDC_TOKEN,
+        &user_ulid,
+        &project_ulid,
+        DbPermissionLevel::ADMIN,
+    )
+    .await;
+
+    let grpc_request = add_token(tonic::Request::new(inner_request.clone()), USER_OIDC_TOKEN);
+
+    let authorizations = service_block
+        .auth_service
+        .get_authorizations(grpc_request)
+        .await
+        .unwrap()
+        .into_inner()
+        .authorizations;
+
+    assert_eq!(authorizations.len(), 1);
+    assert!(authorizations[0].user_permission.contains(&UserPermission {
+        user_id: GENERIC_USER_ULID.to_string(),
+        user_name: "test-user".to_string(),
+        permission_level: PermissionLevel::Admin as i32,
+    }));
+    assert!(authorizations[0].user_permission.contains(&UserPermission {
+        user_id: ADMIN_USER_ULID.to_string(),
+        user_name: "test-admin".to_string(),
+        permission_level: PermissionLevel::Admin as i32,
+    }));
+
+    //ToDo: Get authorization of resource and its subresources
+    //inner_request.recursive = true;
+}
 
 #[tokio::test]
 async fn grpc_delete_authorization() {}
