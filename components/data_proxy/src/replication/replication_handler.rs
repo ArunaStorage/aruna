2d1983665ec3138a6ad9f669dda8b6f5a966c434
lfbrehm <97600985+lfbrehm@users.noreply.github.com>
2023-11-14 10:33:30 +0000

    feat: Task handler draft

--- components/data_proxy/src/replication/replication_handler.rs
+++ components/data_proxy/src/replication/replication_handler.rs
@@ -1,38 +1,83 @@
-use std::sync::Arc;
-
 use ahash::RandomState;
 use anyhow::{anyhow, Result};
 use async_channel::Receiver;
-use dashmap::DashMap;
+use dashmap::DashSet;
 use diesel_ulid::DieselUlid;
+use std::sync::Arc;
+use tracing::error;
+
+use crate::{caching::cache::Cache, database::database::Database};
 
 pub struct ReplicationMessage {
-    object_id: DieselUlid,
-    download_url: String,
+    pub object_id: DieselUlid,
+    pub download_url: String,
+    pub direction: Direction,
+}
+
+#[derive(Debug, Hash, Eq, PartialEq)]
+pub enum Direction {
+    Push,
+    Pull,
 }
 
 pub struct ReplicationHandler {
-    pub reciever: Receiver<ReplicationMessage>,
-    pub queue: Arc<DashMap<DieselUlid, String, RandomState>>,
+    pub receiver: Receiver<ReplicationMessage>,
 }
 
 impl ReplicationHandler {
-    pub fn new(reciever: Receiver<ReplicationMessage>) -> Self {
-        Self {
-            reciever,
-            queue: Arc::new(DashMap::default()),
-        }
+    #[tracing::instrument(level = "trace")]
+    pub fn new(receiver: Receiver<ReplicationMessage>) -> Self {
+        Self { receiver }
     }
-    pub async fn run(&self) -> Result<()> {
-        let recieve = tokio::spawn(async {
+
+    #[tracing::instrument(level = "trace", skip(self, cache, db))]
+    pub async fn run(self, cache: Arc<Cache>, db: Arc<Database>) -> Result<()> {
+        let queue: Arc<DashSet<(DieselUlid, String, Direction), RandomState>> =
+            Arc::new(DashSet::default());
+
+        // Push messages into DashMap for deduplication
+        let queue_clone = queue.clone();
+        let receiver = self.receiver.clone();
+        let recieve = tokio::spawn(async move {
             while let Ok(ReplicationMessage {
                 object_id,
                 download_url,
-            }) = self.reciever.recv().await
+                direction,
+            }) = receiver.recv().await
             {
-                self.queue.insert(object_id, download_url);
+                queue_clone.insert((object_id, download_url, direction));
             }
         });
+
+        // Proccess DashMap entries in separate task
+        let process = tokio::spawn(async move {
+            loop {
+                let next = match queue.iter().next() {
+                    Some(entry) => entry,
+                    None => continue,
+                };
+                let (id, url, direction) = next.key();
+                ReplicationHandler::process(id, url, direction, cache.clone(), db.clone()).await;
+            }
+        });
+        // Run both simultaneous
+        tokio::join!(recieve, process);
+        Err(anyhow!("Not implemented!"))
+    }
+
+    #[tracing::instrument(level = "trace", skip(cache, db))]
+    // TODO
+    // - Pull/Push logic
+    // - write objects into storage backend
+    // - write objects into cache/db
+    async fn process(
+        id: &DieselUlid,
+        url: &str,
+        direction: &Direction,
+        cache: Arc<Cache>,
+        db: Arc<Database>,
+    ) -> Result<()> {
+        error!("Not implemented!");
         Err(anyhow!("Not implemented!"))
     }
 }
