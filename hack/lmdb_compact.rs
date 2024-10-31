2f25ad7d241c28f13c68b9790d8bc834783de88a
Sebastian Beyvers <sb@pus.de>
2024-10-31 15:23:19 +0100

    chore: Added hacks repo for helpers

---
+++ hack/lmdb_compact.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+    let file = "./tests/test_db/store/";
+    let env = unsafe {heed::EnvOpenOptions::new()
+        .map_size(10 * 1024 * 1024)
+        .max_dbs(10)
+        .open(file)
+        .unwrap()};
+
+    env.copy_to_file("./aruna-server/tests/test_db/store/store_small.mdb", heed::CompactionOption::Enabled).unwrap();
+}
